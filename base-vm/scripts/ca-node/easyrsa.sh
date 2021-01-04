5fdd1cd2b3b4fd021e4ac12ba00d8561af343053
Patrick Delamere <patdel76@gmail.com>
2021-01-04 23:28:07 +0100

    Development for EasyRSA and OpenVPN scripts in progress

--- base-vm/scripts/ca-node/easyrsa.sh
+++ base-vm/scripts/ca-node/easyrsa.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -eux
 
-# Update repository
-sudo apt update && apt upgrade -y
-
 # Download Easy RSA
-easyRsaVersion=v3.0.6
+export easyRsaVersion=v3.0.6
 wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/${easyRsaVersion}/EasyRSA-unix-${easyRsaVersion}.tgz
 
 # Untar EasyRSA archive
 tar xvf EasyRSA-unix-${easyRsaVersion}.tgz
-
-
