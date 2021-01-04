a1aaf87744b806b50d795dfb18e1a2e6e4fd9d83
Patrick Delamere <patdel76@gmail.com>
2021-01-04 23:37:58 +0100

    Minor updates to EasyRSA and OpenVPN scripts

--- base-vm/scripts/ca-node/easyrsa.sh
+++ base-vm/scripts/ca-node/easyrsa.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -eux
 
+# Copy scripts to ${vmUser}
+sudo cp -r ${BASE_IMAGE_SSH_USER}/scripts ${VM_USER}
+
 # Download Easy RSA
 export easyRsaVersion=v3.0.6
-wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/${easyRsaVersion}/EasyRSA-unix-${easyRsaVersion}.tgz
+cd /home/${VM_USER}
+wget https://github.com/OpenVPN/easy-rsa/releases/download/${easyRsaVersion}/EasyRSA-unix-${easyRsaVersion}.tgz
 
 # Untar EasyRSA archive
 tar xvf EasyRSA-unix-${easyRsaVersion}.tgz
+
+# Correct permissions
+sudo chown -R ${VM_USER}:${VM_USER} ${VM_USER}
+sudo chmod 700 /home/${VM_USER}/scripts/*.sh
--- base-vm/scripts/vpn-node/openvpn.sh
+++ base-vm/scripts/vpn-node/openvpn.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -eux
 
+# Copy scripts to ${vmUser}
+sudo cp -r ${BASE_IMAGE_SSH_USER}/scripts ${VM_USER}
+
 # Update repository and install OpenVPN
 sudo apt install -y openvpn ufw
 
 # Download Easy RSA
 export easyRsaVersion=v3.0.6
-wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/${easyRsaVersion}/EasyRSA-unix-${easyRsaVersion}.tgz
+cd /home/${VM_USER}
+wget https://github.com/OpenVPN/easy-rsa/releases/download/${easyRsaVersion}/EasyRSA-unix-${easyRsaVersion}.tgz
 
 # Untar EasyRSA archive
 tar xvf EasyRSA-unix-${easyRsaVersion}.tgz
+
+# Correct permissions
+sudo chown -R ${VM_USER}:${VM_USER} ${VM_USER}
+sudo chmod 700 /home/${VM_USER}/scripts/*.sh
 
 # Deploy default OpenVPN configuration
 sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
