689e88afec65fd309fe8093b54d7c2b969ef1046
Patrick Delamere <patdel76@gmail.com>
2021-01-09 00:09:37 +0100

    Minor change to ova export script

--- base-vm/scripts/post-processing/export-vmware-ova.sh
+++ base-vm/scripts/post-processing/export-vmware-ova.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -x
 
-vmName="{{user `vm_name`}}"
-if [[ "${vmName}" =~ "main" ]]; then
-    export outputRootDir="output-main"
-elif [[ "${vmName}" =~ "worker" ]]; then
-    export outputRootDir="output-worker"
-elif [[ "${vmName}" =~ "ca" ]]; then
-    export outputRootDir="output-ca"
-elif [[ "${vmName}" =~ "vpn" ]]; then
-    export outputRootDir="output-vpn"
+if [[ "${VM_NAME}" =~ "main" ]]; then
+    export ${OUTPUT_ROOT_DIR}="output-main"
+elif [[ "${VM_NAME}" =~ "worker" ]]; then
+    export ${OUTPUT_ROOT_DIR}="output-worker"
+elif [[ "${VM_NAME}" =~ "ca" ]]; then
+    export ${OUTPUT_ROOT_DIR}="output-ca"
+elif [[ "${VM_NAME}" =~ "vpn" ]]; then
+    export ${OUTPUT_ROOT_DIR}="output-vpn"
 else
-    echo "Did not recognize built VM -> ${vmName}. Exiting OVA creation script."
+    echo "Did not recognize built VM -> ${VM_NAME}. Exiting OVA creation script."
     exit
 fi
 
 # Create VMWare OVA file
-ovftool --diskMode=thin --compress=9 --name="{{user `vm_name`}}-{{user `version`}}" --prop:vendor="Accenture Interactive" --prop:vendorUrl="https://www.accenture.com" --prop:product="KX.AS.CODE" --prop:productUrl="https://github.com/Accenture/kx.as.code" --prop:version="{{user `version`}}" --prop:fullVersion="{{user `version`}}" --annotation="OVA version of KX.AS.CODE VMWare Image" "../../../${outputRootDir}{{user `vm_suffix`}}/vmware-desktop-{{user `version`}}/{{user `vm_name`}}-{{user `version`}}.vmx" "../../../${outputRootDir}{{user `vm_suffix`}}/vmware-desktop-{{user `version`}}/{{user `vm_name`}}-{{user `version`}}.ova"
+ovftool --diskMode=thin --compress=9 --name="${VM_NAME}-${VM_VERSION}" --prop:vendor="Accenture Interactive" --prop:vendorUrl="https://www.accenture.com" --prop:product="KX.AS.CODE" --prop:productUrl="https://github.com/Accenture/kx.as.code" --prop:version="${VM_VERSION}" --prop:fullVersion="${VM_VERSION}" --annotation="OVA version of KX.AS.CODE VMWare Image" "../../../${${OUTPUT_ROOT_DIR}}${VM_SUFFIX}/vmware-desktop-${VM_VERSION}/${VM_NAME}-${VM_VERSION}.vmx" "../../../${${OUTPUT_ROOT_DIR}}${VM_SUFFIX}/vmware-desktop-${VM_VERSION}/${VM_NAME}-${VM_VERSION}.ova"
