eb1b9ceb32d5407fdec53d15d9ae5c872fe1ed57
Patrick Delamere <patdel76@gmail.com>
2021-01-27 22:08:10 +0100

    Changed method for creating Gitlab personal access token

--- auto-setup/cicd/gitlab-ce/post_install_scripts/createLoginToken.sh
+++ auto-setup/cicd/gitlab-ce/post_install_scripts/createLoginToken.sh
@@ -12,7 +12,7 @@ if [[ ! -f /home/${vmUser}/.config/kx.as.code/.admin.gitlab.pat ]]; then
   export personalAccessToken=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 
   # Save generated token to admin user account
-  kubectl exec -n ${namespace} ${podName} -- gitlab-rails runner "token = User.find_by_username('${adminUser}').personal_access_tokens.create(scopes: [:api], name: 'Automation token'); token.set_token('${personalAccessToken}'); token.save\!"
+  kubectl exec -n ${namespace} ${podName} -- gitlab-rails runner "token = User.find_by_username('${adminUser}').personal_access_tokens.create(scopes: [:api], name: 'Automation token'); token.set_token('${personalAccessToken}'); token.save!"
 
   # Save token to file for use in later script executions
   echo ${personalAccessToken} | sudo tee /home/${vmUser}/.config/kx.as.code/.admin.gitlab.pat
