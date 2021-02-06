52a518295a178b3de56a1dcdcc7b71215af9ee6a
Patrick Delamere <patdel76@gmail.com>
2021-02-06 16:00:45 +0100

    Added keycloak for central IAM

--- base-vm/scripts/main-node/keycloak.sh
+++ base-vm/scripts/main-node/keycloak.sh
@@ -5,19 +5,18 @@ export keyCloakVersion=12.0.2
 
 # Download and Install Keyclok
 sudo wget https://github.com/keycloak/keycloak/releases/download/${keyCloakVersion}/keycloak-${keyCloakVersion}.tar.gz
-https://github.com/keycloak/keycloak/releases/download/12.0.2/keycloak-12.0.2.tar.gz
 sudo tar -xzf keycloak-${keyCloakVersion}.tar.gz -C /opt/
 sudo mv /opt/keycloak-${keyCloakVersion} /opt/keycloak
 
 # Add User and Group for Keycloak
-groupadd keycloak
-useradd -r -g keycloak -d /opt/keycloak -s /sbin/nologin keycloak
+sudo groupadd keycloak
+sudo useradd -r -g keycloak -d /opt/keycloak -s /sbin/nologin keycloak
 
 # Create Keycloak config files
-mkdir -p /etc/keycloak
-cp /opt/keycloak/docs/contrib/scripts/systemd/wildfly.conf /etc/keycloak/keycloak.conf
-cp /opt/keycloak/docs/contrib/scripts/systemd/launch.sh /opt/keycloak/bin/
-cp /opt/keycloak/docs/contrib/scripts/systemd/wildfly.service /etc/systemd/system/keycloak.service
+sudo mkdir -p /etc/keycloak
+sudo cp /opt/keycloak/docs/contrib/scripts/systemd/wildfly.conf /etc/keycloak/keycloak.conf
+sudo cp /opt/keycloak/docs/contrib/scripts/systemd/launch.sh /opt/keycloak/bin/
+sudo cp /opt/keycloak/docs/contrib/scripts/systemd/wildfly.service /etc/systemd/system/keycloak.service
 sudo sed -i 's/WILDFLY_HOME=\"\/opt\/wildfly\"/WILDFLY_HOME=\"\/opt\/keycloak\"/g' /opt/keycloak/bin/launch.sh
 sudo sed -i 's/jboss.http.port:8080/jboss.http.port:9080/g' /opt/keycloak/standalone/configuration/standalone.xml
 sudo sed -i 's/jboss.https.port:8443/jboss.https.port:9443/g' /opt/keycloak/standalone/configuration/standalone.xml
@@ -42,20 +41,20 @@ WantedBy=multi-user.target
 """ | sudo tee /etc/systemd/system/keycloak.service
 
 # Correct permissions
-chown keycloak:keycloak -R /opt/keycloak
-chmod o+x /opt/keycloak/bin
+sudo chown keycloak:keycloak -R /opt/keycloak
+sudo chmod o+x /opt/keycloak/bin
 
 # Enable Keycloak service
-systemctl daemon-reload
-systemctl enable keycloak
-systemctl start keycloak
-systemctl status keycloak
+sudo systemctl daemon-reload
+sudo systemctl enable keycloak
+sudo systemctl start keycloak
+sudo systemctl status keycloak
 
 # Create initial default admin user
-/opt/keycloak/bin/add-user-keycloak.sh -r master -u $VM_USER -p $VM_PASSWORD
+sudo /opt/keycloak/bin/add-user-keycloak.sh -r master -u $VM_USER -p $VM_PASSWORD
 
 # Restart Keycloak
-systemctl restart keycloak
+sudo systemctl restart keycloak
 
 # Create Desktop Icon
 # Install Desktop Shortcut
@@ -65,7 +64,7 @@ Version=1.0
 Name=RabbitMQ
 GenericName=RabbitMQ
 Comment=RabbitMQ Action Queues
-Exec=/usr/bin/google-chrome-stable %U http://localhost:9080/ --use-gl=angle --password-store=basic
+Exec=/usr/bin/google-chrome-stable %U http://localhost:9080/auth --use-gl=angle --password-store=basic
 StartupNotify=true
 Terminal=false
 Icon=/usr/share/kx.as.code/git/kx.as.code/base-vm/images/keycloak.png
@@ -73,8 +72,8 @@ Type=Application
 Categories=Development
 MimeType=text/html;text/xml;application/xhtml_xml;image/webp;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;
 Actions=new-window;new-private-window;
-''' | sudo tee /home/${vmUser}/Desktop/RabbitMQ.desktop
+''' | sudo tee /home/${$VM_USER}/Desktop/Keycloak.desktop
 
 # Give *.desktop files execute permissions
-sudo chmod 755 /home/${vmUser}/Desktop/RabbitMQ.desktop
-sudo chown ${vmUser}:${vmUser} /home/${vmUser}/Desktop/RabbitMQ.desktop
+sudo chmod 755 /home/${$VM_USER}/Desktop/Keycloak.desktop
+sudo chown ${$VM_USER}:${$VM_USER} /home/${$VM_USER}/Desktop/Keycloak.desktop
