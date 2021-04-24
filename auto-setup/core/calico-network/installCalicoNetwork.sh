76516737a107fa22b40315355db1064d2e2eaaa6
Patrick Delamere <patdel76@gmail.com>
2021-04-24 10:32:49 +0200

    Updated Calico network install script

--- auto-setup/core/calico-network/installCalicoNetwork.sh
+++ auto-setup/core/calico-network/installCalicoNetwork.sh
@@ -2,6 +2,6 @@
 
 # Install Calico Network
 curl https://docs.projectcalico.org/v3.17/manifests/calico.yaml --output ${installationWorkspace}/calico.yaml
-sed -i -e '/^            - name: FELIX_HEALTHENABLED/{:a; N; /\n              value: "true"/!ba; a\            - name: IP_AUTODETECTION_METHOD\n              value: "interface='${netDevice}'"' -e '}' calico.yaml
+sed -i -e '/^            - name: FELIX_HEALTHENABLED/{:a; N; /\n              value: "true"/!ba; a\            - name: IP_AUTODETECTION_METHOD\n              value: "interface='${netDevice}'"' -e '}' ${installationWorkspace}/calico.yaml
 kubectl apply -f ${installationWorkspace}/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
