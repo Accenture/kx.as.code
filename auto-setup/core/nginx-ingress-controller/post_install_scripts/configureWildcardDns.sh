ab73b58d301848b1e9952fe92689c5de32482f25
Patrick Delamere <patdel76@gmail.com>
2021-04-24 18:39:43 +0200

    Updated DNSMasq Wildcard DNS config

--- auto-setup/core/nginx-ingress-controller/post_install_scripts/configureWildcardDns.sh
+++ auto-setup/core/nginx-ingress-controller/post_install_scripts/configureWildcardDns.sh
@@ -2,5 +2,7 @@
 
 # Update Wildcard DNS entry for kx-as-code.local
 export nginxIngressControllerIp=$(kubectl get svc ${namespace}-ingress-nginx-controller -n ${namespace} -o jsonpath={.spec.clusterIP})
-echo "address=/.${baseDomain}/${nginxIngressControllerIp}" | tee -a /etc/dnsmasq.d/${baseDomain}.conf
+# Switched from NGINX LB IP to Host IP due to Host Networking enabled for AWS solution
+# Will revisit this in future if we add additional main nodes
+echo "address=/.${baseDomain}/${mainIpAddress}" | tee -a /etc/dnsmasq.d/${baseDomain}.conf
 systemctl restart dnsmasq
