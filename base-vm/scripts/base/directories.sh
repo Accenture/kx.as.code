8f9cacb6040d2fe2bbdbef07e9e68cdbe9400257
Patrick Delamere <patdel76@gmail.com>
2021-05-13 00:23:36 +0000

    Modified packer build scripts

---
+++ base-vm/scripts/base/directories.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+
+sudo mkdir -p ${SKELDIR} && sudo chmod 777 ${SKELDIR}
+sudo mkdir -p ${SHARED_GIT_REPOSITORIES} && sudo chmod 777 ${SHARED_GIT_REPOSITORIES}
+sudo mkdir -p ${INSTALLATION_WORKSPACE} && sudo chmod 777 ${INSTALLATION_WORKSPACE}
+
