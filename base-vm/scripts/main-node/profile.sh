ae87ebeed6f4718ab8e6d290ceb679c3254c4d80
Patrick Delamere <patdel76@gmail.com>
2021-06-10 19:04:29 +0200

    Moved .desktop file copy to later script

--- base-vm/scripts/main-node/profile.sh
+++ base-vm/scripts/main-node/profile.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -eux
 
 # Copy Skel
-sudo cp -rfT ${INSTALLATION_WORKSPACE}/skel /home/$VM_USER
+sudo cp -rfT ${INSTALLATION_WORKSPACE}/skel /home/${VM_USER}
 sudo cp -rfT ${INSTALLATION_WORKSPACE}/skel /root
-sudo chown -R $VM_USER:$VM_USER /home/$VM_USER
-sudo chmod -R 755 /home/$VM_USER/Desktop/*.desktop
+sudo chown -R ${VM_USER}:${VM_USER} /home/${VM_USER}
+sudo chmod -R 755 /home/${VM_USER}/Desktop/*.desktop
 
 # Update system desktop icon file for HTOP
-sudo cp /home/$VM_USER/Desktop/HTOP.desktop /usr/share/applications/htop.desktop
+sudo cp -f /home/${VM_USER}/Desktop/HTOP.desktop /usr/share/applications/htop.desktop
+sudo cp -f /home/${VM_USER}/Desktop/Tilix.desktop /usr/share/applications/com.gexperts.Tilix.desktop
 
 # Change screen resolution to more respectable 1920x1200 (default is 800x600)
 sudo bash -c 'cat <<EOF > /etc/X11/xorg.conf
@@ -33,20 +34,20 @@ Section "Screen"
 EndSection
 EOF'
 
-sudo mkdir -p /home/$VM_USER/.config
-sudo chown -R $VM_USER:$VM_USER /home/$VM_USER/.config
+sudo mkdir -p /home/${VM_USER}/.config
+sudo chown -R ${VM_USER}:${VM_USER} /home/${VM_USER}/.config
 
 # Source VTE config (for Tilix) in .zshrc
 echo -e '\n# Fix for Tilix\nif [ $TILIX_ID ] || [ $VTE_VERSION ]; then
         source /etc/profile.d/vte.sh
-fi' | sudo tee -a /home/$VM_USER/.zshrc
+fi' | sudo tee -a /home/${VM_USER}/.zshrc
 
 # Ensure users permissions are correct
-sudo chown -R $VM_USER:$VM_USER /home/$VM_USER
+sudo chown -R ${VM_USER}:${VM_USER} /home/${VM_USER}
 
 # Remove ZSH adding % to output with no new-line character
-echo "export PROMPT_EOL_MARK=''" | sudo tee -a /home/$VM_USER/.zshrc
-echo ". ./p10k.zsh" | sudo tee -a /home/$VM_USER/.zshrc
+echo "export PROMPT_EOL_MARK=''" | sudo tee -a /home/${VM_USER}/.zshrc
+echo ". ./p10k.zsh" | sudo tee -a /home/${VM_USER}/.zshrc
 
 # Copy avatar images to shared directory
 sudo mkdir -p /usr/share/avatars
@@ -54,9 +55,9 @@ sudo cp -r ${INSTALLATION_WORKSPACE}/theme/avatars /usr/share
 
 # Assign random avatar to kx.hero user
 ls /usr/share/avatars/avatar_*.png | sort -R | tail -1 | while read file; do
-    sudo cp -f $file /home/$VM_USER/.face.icon
+    sudo cp -f $file /home/${VM_USER}/.face.icon
 done
-sudo chown $VM_USER:$VM_USER /home/$VM_USER/.face.icon
+sudo chown ${VM_USER}:${VM_USER} /home/${VM_USER}/.face.icon
 
 # Add keyboard layouts so they can be selected from XFCE4 panel
 sudo bash -c 'cat <<EOF > /etc/default/keyboard
@@ -72,7 +73,7 @@ XKBOPTIONS=""
 BACKSPACE="guess"'
 
 # Add load of global variables to bashrc and zshrc
-echo -e "\nsource /etc/environment" | sudo tee -a /home/$VM_USER/.bashrc /home/$VM_USER/.zshrc /root/.bashrc /root/.zshrc
+echo -e "\nsource /etc/environment" | sudo tee -a /home/${VM_USER}/.bashrc /home/${VM_USER}/.zshrc /root/.bashrc /root/.zshrc
 
 # Hide Vagrant user from Login screen
 echo '''[Autologin]
