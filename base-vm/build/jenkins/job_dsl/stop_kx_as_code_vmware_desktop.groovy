381e8fac7313d147cb14e97b9728b0e0126cd17d
Patrick Delamere <patdel76@gmail.com>
2021-06-12 16:55:24 +0200

    Minor fix

--- base-vm/build/jenkins/job_dsl/stop_kx_as_code_vmware_desktop.groovy
+++ base-vm/build/jenkins/job_dsl/stop_kx_as_code_vmware_desktop.groovy
@@ -3,7 +3,7 @@ import hudson.model.Item
 import hudson.model.Items
 
 def jobProperties
-Item currentJob = Jenkins.instance.getItemByFullName('VirtualBox/GeneratedJobs/04_Stop_KX.AS.CODE')
+Item currentJob = Jenkins.instance.getItemByFullName('VMWare_Workstation/GeneratedJobs/04_Stop_KX.AS.CODE')
 if (currentJob) {
   jobProperties = currentJob.@properties
 }
@@ -12,13 +12,13 @@ jobProperties.each {
    println "${it.dump()}"
 }
 
-pipelineJob('VirtualBox/GeneratedJobs/04_Stop_KX.AS.CODE') {
+pipelineJob('VMWare_Workstation/GeneratedJobs/04_Stop_KX.AS.CODE') {
     definition {
       cps {
         script(readFileFromWorkspace('base-vm/build/jenkins/pipelines/stop-kx.as.code-vmware-desktop.Jenkinsfile'))
         sandbox()
       }
-      queue("VirtualBox/GeneratedJobs/04_Stop_KX.AS.CODE")
+      queue("VMWare_Workstation/GeneratedJobs/04_Stop_KX.AS.CODE")
       if (jobProperties) {
       configure { root ->
         def properties = root / 'properties'
