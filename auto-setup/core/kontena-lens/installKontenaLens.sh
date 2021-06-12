3ad88786a17d02e3a7f441344128cd22d0d3172b
Patrick Delamere <patdel76@gmail.com>
2021-06-12 15:27:04 +0200

    Fixed Kontena Lens installation

--- auto-setup/core/kontena-lens/installKontenaLens.sh
+++ auto-setup/core/kontena-lens/installKontenaLens.sh
@@ -47,10 +47,30 @@ echo '''
 	"currentWorkspace": "default"
 }''' | sudo tee /home/${vmUser}/.config/Lens/lens-workspace-store.json
 
-sudo chown ${vmUser}:${vmUser} /home/${vmUser}/.config/Lens/lens-cluster-store.json /home/${vmUser}/.config/Lens/lens-cluster-store.json
-
-curl -o ${installationWorkspace}/Lens-${lensVersion}.amd64.deb https://github.com/lensapp/lens/releases/download/v${lensVersion}/Lens-${lensVersion}.amd64.deb
+sudo chown -R ${vmUser}:${vmUser} /home/${vmUser}/.config/Lens
+sudo curl -L -o ${installationWorkspace}/Lens-${lensVersion}.amd64.deb https://github.com/lensapp/lens/releases/download/v${lensVersion}/Lens-${lensVersion}.amd64.deb
 sudo apt-get install -y ${installationWorkspace}/Lens-${lensVersion}.amd64.deb
-cp /usr/share/applications/kontena-lens.desktop /home/${vmUser}/Desktop
+
+echo '''[Desktop Entry]
+Categories=Network;
+Comment[en_US]=Lens - The Kubernetes IDE
+Comment=Lens - The Kubernetes IDE
+Exec=/opt/Lens/kontena-lens %U
+GenericName[en_US]=
+GenericName=
+Icon=kontena-lens
+MimeType=
+Name=Lens
+Path=
+StartupNotify=true
+StartupWMClass=Lens
+Terminal=false
+TerminalOptions=
+Type=Application
+X-DBUS-ServiceName=
+X-DBUS-StartupType=
+X-KDE-SubstituteUID=false
+X-KDE-Username=
+''' | sudo tee /home/${vmUser}/Desktop/kontena-lens.desktop
 sudo chmod 755 /home/${vmUser}/Desktop/kontena-lens.desktop
 sudo chown ${vmUser}:${vmUser} /home/${vmUser}/Desktop/kontena-lens.desktop
