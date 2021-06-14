c74512ebd8302e48ea8c302e0e7650b234408713
Kanwaljit Kaur <kanwaljit.kaur@accenture.com>
2021-06-14 08:42:51 +0000

    Combining the configuration scripts for harbor and keycloak

--- auto-setup/cicd/harbor/post_install_scripts/deployOidc.sh
+++ auto-setup/cicd/harbor/post_install_scripts/deployOidc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
-# Settings on Keycloak for SSO 
+# Settings on Keycloak for SSO
+ 
 export kcRealm=${baseDomain}
 export kcInternalUrl=http://localhost:8080
 export kcAdmCli=/opt/jboss/keycloak/bin/kcadm.sh
@@ -13,8 +14,8 @@ kubectl -n keycloak exec ${kcPod} -- \
 ## create a clients
 kubectl -n keycloak exec ${kcPod} -- \
 ${kcAdmCli} create clients --realm ${kcRealm} -s clientId=${componentName} \
--s 'redirectUris=["https://'${componentName}'.'${baseDomain}'/auth/callback"]' \
--s publicClient="false" -s enabled=true -s rootUrl="https://'${componentName}'.'${baseDomain}'" -s baseUrl="/applications" -i 
+-s 'redirectUris=["https://'${componentName}'.'${kcRealm}'/c/oidc/callback"]' \
+-s publicClient="false" -s enabled=true -s rootUrl="https://'${componentName}'.'${kcRealm}'" -s baseUrl="/applications" -i 
 
 ## export clientId
 export clientID=$(kubectl -n keycloak exec ${kcPod} -- \
@@ -45,3 +46,45 @@ ${kcAdmCli}  create client-scopes/$clientscopeID/protocol-mappers/models \
 ## map the above client scope id to the client 
 kubectl -n keycloak exec ${kcPod} -- \
 ${kcAdmCli}  update clients/$clientID/default-client-scopes/$clientscopeID
+
+###################################################OIDC Configuration Harbor#########################################################
+
+# Wait until API is available before continuing
+ timeout -s TERM 600 bash -c 'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' https://'${componentName}'.'${baseDomain}'/api/v2.0/projects)" != "200" ]]; do \
+                             echo "Waiting for https://'${componentName}'.'${baseDomain}'/api/v2.0/projects"; sleep 5; done'
+
+# get the configuration
+export harborAuthMode=$(curl -u 'admin:'${vmPassword}'' -H "Content-Type: application/json" -X GET "https://${componentName}.${baseDomain}/api/v2.0/configurations" | jq -r '.auth_mode.value')
+if [[ ${harborAuthMode} != "oidc_auth" ]];
+then
+# set the oidc configuration
+   export harborAuthModeResp=$(curl -u 'admin:'${vmPassword}'' -X PUT "https://${componentName}.${baseDomain}/api/v2.0/configurations" -H "accept: application/json" -H "Content-Type: application/json" -d '{
+   "auth_mode":"oidc_auth",
+   "oidc_name":"Keycloak Auth",
+   "oidc_endpoint":"https://keycloak.'${baseDomain}'/auth/realms/'${kcRealm}'",
+   "oidc_client_id":"'${componentName}'",
+   "oidc_client_secret":"'${clientSecret}'",
+   "oidc_scope":"openid,profile,email,offline_access",
+   "oidc_groups_claim":"groups",
+   "oidc_verify_cert": "false",
+   "oidc_auto_onboard":"true",
+   "oidc_user_claim":"preferred_username"
+   }')
+   if [[ ${harborAuthModeResp} -eq 200 ]] || [[ ${harborOidcPingRes} = '' ]];
+   then
+      echo "Successfully created oidc configuration"
+   else echo  "Unable to create configuration"
+   fi
+ else echo "Harbor oidc configuration already exists. Skipping creation"
+fi
+
+# Test the oidc configuration
+export harborOidcPingRes=$(curl -u 'admin:'${vmPassword}'' -X POST "https://${componentName}.${baseDomain}/api/v2.0/system/oidc/ping" -H "accept: application/json" -H "Content-Type: application/json" -d '
+{ "url":"https://keycloak.'${baseDomain}'/auth/realms/'${kcRealm}'",
+  "verify_cert":false
+}' )
+if [[ ${harborOidcPingRes} -eq 200 ]] ||  [[ ${harborOidcPingRes} = '' ]];
+then
+    echo  "OIDC Test Connection Successful"
+else echo  "Fail to Connect"
+fi
