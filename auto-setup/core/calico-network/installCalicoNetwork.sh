d0234e06e0073d27e5cf8a18792fa32822f777b1
Patrick Delamere <patdel76@gmail.com>
2021-08-01 11:25:09 +0200

    Updated Calico version

--- auto-setup/core/calico-network/installCalicoNetwork.sh
+++ auto-setup/core/calico-network/installCalicoNetwork.sh
@@ -8,7 +8,11 @@ do sleep 5;\
 done'
 
 # Install Calico Network
-curl https://docs.projectcalico.org/v3.17/manifests/calico.yaml --output ${installationWorkspace}/calico.yaml
+curl https://docs.projectcalico.org/${calicoVersion}/manifests/calico.yaml --output ${installationWorkspace}/calico.yaml
 sed -i -e '/^            - name: FELIX_HEALTHENABLED/{:a; N; /\n              value: "true"/!ba; a\            - name: IP_AUTODETECTION_METHOD\n              value: "interface='${netDevice}'"' -e '}' ${installationWorkspace}/calico.yaml
 kubectl apply -f ${installationWorkspace}/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+
+# Install CalicoCtl
+/usr/bin/sudo curl -o /usr/bin/calicoctl -O -L  "https://github.com/projectcalico/calicoctl/releases/download/${calicoCtlVersion}/calicoctl"
+/usr/bin/sudo chmod +x /usr/bin/calicoctl
