b537c8e11899bf33bb3c8718e89ef7336b867ff5
Patrick Delamere <patdel76@gmail.com>
2021-08-02 22:31:15 +0200

    Fixed rabbitmqadmin bash completion

--- auto-setup/pollActionQueue.sh
+++ auto-setup/pollActionQueue.sh
@@ -406,7 +406,7 @@ if [ ! -f /usr/local/bin/rabbitmqadmin ]; then
     chmod +x rabbitmqadmin
     mv rabbitmqadmin /usr/local/bin/rabbitmqadmin
     # Add bash auto-completion
-    sudo rabbitmqadmin --bash-completion | sudo tee /etc/bash_completion.d/rabbitmqadmin
+    rabbitmqadmin --bash-completion | sudo tee /etc/bash_completion.d/rabbitmqadmin
     echo "source /etc/bash_completion.d/rabbitmqadmin" | sudo tee -a /home/${VM_USER}/.bashrc /home/${VM_USER}/.zshrc /root/.bashrc /root/.zshrc
 fi
 
