b9317251b79fb32f644955f09063a1330b0b9de0
Patrick Delamere <patdel76@gmail.com>
2021-08-07 20:45:44 +0200

    Minor change

--- base-vm/scripts/worker-node/k8s-base.sh
+++ base-vm/scripts/worker-node/k8s-base.sh
@@ -34,11 +34,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 # Read Kubernetes version to be installed
-kubeVersion=$(cat ${installationWorkspace}/versions.json | jq -r '.kubernetes')
 apt-get update
-apt-get install -y kubelet=${kubeVersion} kubeadm=${kubeVersion} kubectl=${kubeVersion}
-apt-mark hold kubelet kubeadm kubectl
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION}
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # Switch off swap
