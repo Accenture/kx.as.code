f2f62d942b58969572636c07a7c02e9792036961
Manoj Reddy Yalamareddy <manoj.r.yalamareddy@accenture.com>
2021-09-01 10:39:29 +0200

    gitlab sso config

--- auto-setup/core/cfssl-certificate-authority/installCfsslCertificateAuthority.sh
+++ auto-setup/core/cfssl-certificate-authority/installCfsslCertificateAuthority.sh
@@ -128,13 +128,11 @@ EOF
 
     # Generate Server Certificates
     cfssl gencert -ca kx_intermediate_ca.pem -ca-key kx_intermediate_ca-key.pem -config intermediate_to_client_cert.json csr_kx_server.json | cfssljson -bare kx_server
-    # Create certificate chain
-    cat ${certificatesWorkspace}/kx_server.pem ${certificatesWorkspace}/kx_intermediate_ca.pem ${certificatesWorkspace}/kx_root_ca.pem | /usr/bin/sudo tee ${certificatesWorkspace}/kx_chain_bundle.pem
     # Prepare Certificates for Kubernetes Secrets Import
     mkdir -p ${installationWorkspace}/kx-certs
     /usr/bin/sudo cp ${certificatesWorkspace}/kx_intermediate_ca.pem ${installationWorkspace}/kx-certs/ca.crt
     /usr/bin/sudo cp ${certificatesWorkspace}/kx_server-key.pem ${installationWorkspace}/kx-certs/tls.key
-    /usr/bin/sudo cp ${certificatesWorkspace}/kx_chain_bundle.pem ${installationWorkspace}/kx-certs/tls.crt
+    /usr/bin/sudo cp ${certificatesWorkspace}/kx_server.pem ${installationWorkspace}/kx-certs/tls.crt
     /usr/bin/sudo chown -R ${vmUser}:${vmUser} ${installationWorkspace}/kx-certs
 
     # Import Certificates into Browser Certificate Repositories
