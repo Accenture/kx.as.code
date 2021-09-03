d57e0849c7b925133e10b47c74799e41389e1ad7
Manoj Reddy Yalamareddy <manoj.r.yalamareddy@accenture.com>
2021-09-03 15:32:43 +0200

    gitlab sso config

--- auto-setup/cicd/gitlab-ce/pre_install_scripts/createOAuth.sh
+++ auto-setup/cicd/gitlab-ce/pre_install_scripts/createOAuth.sh
@@ -34,52 +34,28 @@ fi
 
 ################### kubernetes manifests CRUD operations #####################################
 
-# kubectl get cm gitlab-ce-webservice -n gitlab-ce -o json > /home/kx.hero/cm.json
-
-# # This is a ugly hack which should be improvised if a better alternative is found. From the cm.json file we will perform some file modifications
-# # by removing gitlab.yml.erb section first where the sso keycloak config goes. Now we have another file called oAuthSupport
-# # which has this config already but with dummy values, those dummy value will be replaced with actual values and remove gitlab.yml.erb line
-# # will be added again into cm.json and applied with kubectl
-
-# /usr/bin/sudo sed -i '/gitlab.yml.erb/d' /home/kx.hero/cm.json
-# /usr/bin/sudo sed -ie "s/DummyIssuer/${baseDomain}/g" ${autoSetupHome}/cicd/gitlab-ce/post_install_scripts/oAuthSupport.json
-# /usr/bin/sudo sed -ie "s/DummyRealm/${baseDomain}/g" ${autoSetupHome}/cicd/gitlab-ce/post_install_scripts/oAuthSupport.json
-# /usr/bin/sudo sed -ie "s/DummySecret/${clientSecret}/g" ${autoSetupHome}/cicd/gitlab-ce/post_install_scripts/oAuthSupport.json
-# /usr/bin/sudo sed -ie "s/DummyRedirectUri/gitlab.${baseDomain}/g" ${autoSetupHome}/cicd/gitlab-ce/post_install_scripts/oAuthSupport.json
-# /usr/bin/sudo sed -ie "s/DummyIdentifier/${componentName}/g" ${autoSetupHome}/cicd/gitlab-ce/post_install_scripts/oAuthSupport.json
-# /usr/bin/sudo sed -i "/installation_type/r ${autoSetupHome}/cicd/gitlab-ce/post_install_scripts/oAuthSupport.json" /home/kx.hero/cm.json
-
-# kubectl apply -f /home/kx.hero/cm.json
-
-# kubectl delete pod -l  app=webservice -n gitlab-ce
-
-
-#kubectl create ns ${componentName}
-
-echo """
-name: 'openid_connect'
-label: 'Keycloak'
-args:
-  name: openid_connect
-label: Keycloak
-args:
-  name: openid_connect
-  scope:
-    - openid
-    - profile
-    - email
-  response_type: code
-  issuer: 'https://keycloak.${baseDomain}/auth/realms/${baseDomain}'
-  discovery: true
-  client_auth_method: query
-  send_scope_to_token_endpoint: false
-  user_response_structure:
-    id_path: preferred_username
-    attributes:
-      nickname: preferred_username
-  client_options:
-    identifier: '${clientId}'
-    secret: '${clientSecret}'
-    redirect_uri: 'https://gitlab.${baseDomain}/users/auth/openid_connect/callback'
-""" | /usr/bin/sudo tee ${installationWorkspace}/gitlab-sso-providers.yaml
+echo '''
+{
+  "name": "openid_connect",
+  "label": "Keycloak",
+  "args": {
+    "name": "openid_connect",
+    "scope": ["openid","profile","email"],
+    "response_type": "code",
+    "issuer": "https://keycloak.'${baseDomain}'/auth/realms/'${baseDomain}'",
+    "discovery": true,
+    "client_auth_method": "query",
+    "send_scope_to_token_endpoint": false,
+    "user_response_structure": {
+      "id_path": "preferred_username",
+      "attributes": {  "nickname":  "preferred_username"  }
+    },
+    "client_options": {
+      "identifier": "gitlab-ce",
+      "secret": "'${clientSecret}'",
+      "redirect_uri": "https://gitlab.'${baseDomain}'/users/auth/openid_connect/callback"
+    }
+  }
+}
+''' | /usr/bin/sudo tee ${installationWorkspace}/gitlab-sso-providers.yaml
 kubectl create secret generic sso-provider --namespace=${componentName} --from-file=provider=${installationWorkspace}/gitlab-sso-providers.yaml
