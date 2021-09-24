3a395f849813761a5ce9c314010587672dd4aade
Manoj Reddy Yalamareddy <manoj.r.yalamareddy@accenture.com>
2021-09-24 15:18:28 +0200

    initial loki config

--- auto-setup/monitoring/loki/pre_install_scripts/getGrafanaAccessKey.sh
+++ auto-setup/monitoring/loki/pre_install_scripts/getGrafanaAccessKey.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 # Get Grafana username and password
-export grafanaUser=$(kubectl get secret grafana-admin-credentials -n monitoring -o json | jq -r '.data.admin-user' | base64 --decode)
-export grafanaPassword=$(kubectl get secret grafana-admin-credentials -n monitoring -o json | jq -r '.data.admin-password' | base64 --decode)
+export grafanaUser=$(kubectl get secret grafana-admin-credentials -n monitoring -o jsonpath='{.data.admin-user}' | base64 --decode)
+export grafanaPassword=$(kubectl get secret grafana-admin-credentials -n monitoring -o jsonpath='{.data.admin-password}' | base64 --decode)
