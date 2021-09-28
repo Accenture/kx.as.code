e08eb9f1360954fd624cae9a97d1a18ce79e06be
Patrick Delamere <patdel76@gmail.com>
2021-09-28 05:46:39 +0000

    Added central functions

--- auto-setup/collaboration/mattermost/post_install_scripts/createInitialAdminUser.sh
+++ auto-setup/collaboration/mattermost/post_install_scripts/createInitialAdminUser.sh
@@ -7,8 +7,11 @@ if [[ -z ${mattermostLoginToken} ]]; then
     # Get pod for running CLI commands
     mattermostPod=$(kubectl get pod -n ${namespace} -l app.kubernetes.io/name=mattermost-team-edition  --output=name)
 
+    # Generate secure admin password
+    generatedPassword=$(generateSecurePassword)
+
     # Create initial admin user
-    kubectl -n ${namespace} exec ${mattermostPod} -- bin/mattermost user create --firstname admin --system_admin --email admin@${baseDomain} --username admin --password ${vmPassword}
+    kubectl -n ${namespace} exec ${mattermostPod} -- bin/mattermost user create --firstname admin --system_admin --email admin@${baseDomain} --username admin --password ${generatedPassword}
 else
     log_info "Mattermost admin user already exists. Skipping creation"
 fi
---
+++ auto-setup/functions/notify.sh
@@ -0,0 +1,17 @@
+notify() {
+  openDisplays=$(w -oush | grep -Eo ' :[0-9]+' | sort -u -t\  -k1,1 | cut -d \  -f 2 || true)
+  log_info "Detected unique displays: ${openDisplays}"
+  messageTimeout=300000
+  messageTitle="KX.AS.CODE Notification"
+  message=${1}
+  messageType=${2}
+  for display in ${openDisplays}; do
+    displayUser=$(w -oush | grep -sw "${display}" | awk {'print $1'} | uniq)
+    echo "Sending notification to display ${display} for user ${displayUser}"
+    if [[ -S "/run/user/$(id -u ${displayUser})/bus" ]]; then
+        /usr/bin/sudo -H -i -u ${displayUser} bash -c "DISPLAY=\"${display}\" \
+        DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ${displayUser})/bus \
+        notify-send -t \"${messageTimeout}\" \"${messageTitle}\" \"${message}\" --icon=\"${messageType}\""
+    fi
+  done
+}
