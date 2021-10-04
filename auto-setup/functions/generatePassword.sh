15054d441ca23a8aaf5b05bd8b87792be341fd68
Patrick Delamere <patdel76@gmail.com>
2021-10-04 12:50:58 +0000

    Removed $ from possible password characters

--- auto-setup/functions/generatePassword.sh
+++ auto-setup/functions/generatePassword.sh
@@ -1,6 +1,6 @@
 generatePassword() {
   # Generate a password and store it in GoPass
-  chars='@#$%&_+='
+  chars='#%-=.*&_'
   { </dev/urandom LC_ALL=C grep -ao '[A-Za-z0-9]' \
           | head -n$((RANDOM % 8 + 9))
       echo ${chars:$((RANDOM % ${#chars})):1}   # Random special char.
