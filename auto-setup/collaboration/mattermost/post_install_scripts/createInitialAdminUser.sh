8cbc5c15153d59ad8181aae74b5c90b7f795d27d
kanwaljit kaur <kanwaljit.kaur@accenture.com>
2021-10-11 04:09:36 +0000

    Changed code with respect to new password management

--- auto-setup/collaboration/mattermost/post_install_scripts/createInitialAdminUser.sh
+++ auto-setup/collaboration/mattermost/post_install_scripts/createInitialAdminUser.sh
@@ -7,11 +7,11 @@ if [[ -z ${mattermostLoginToken} ]]; then
     # Get pod for running CLI commands
     mattermostPod=$(kubectl get pod -n ${namespace} -l app.kubernetes.io/name=mattermost-team-edition  --output=name)
 
-    # Generate secure admin password
-    generatedPassword=$(generateSecurePassword)
-
+    # Get secure admin password
+    export getPassword=$(getPassword "mattermost-admin-password")
+    
     # Create initial admin user
-    kubectl -n ${namespace} exec ${mattermostPod} -- bin/mattermost user create --firstname admin --system_admin --email admin@${baseDomain} --username admin --password ${generatedPassword}
+    kubectl -n ${namespace} exec ${mattermostPod} -- bin/mattermost user create --firstname admin --system_admin --email admin@${baseDomain} --username admin --password ${getPassword}
 else
     log_info "Mattermost admin user already exists. Skipping creation"
 fi
