0653ab0e515d4cefc8ad2ec2e618a7a454c67a02
Patrick Delamere <patdel76@gmail.com>
2021-10-11 17:17:16 +0000

    Added HTML font family to job description

--- base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
+++ base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
@@ -23,8 +23,8 @@ def setBuildEnvironment() {
             } else {
                 println("Kube version has not changed since the last build - v${kube_version}")
             }
-            gitDiff = sh (script: "git log ${oldGitShortCommitId}..${gitShortCommitId} --oneline --graph --decorate --no-merges --stat --pretty='format:%C(auto)%<(15,trunc)%h %<(15,trunc)%ar %<(20,trunc)%cn  %C(Cyan)%<(100,trunc)%s%C(reset)'", returnStdout: true).trim()
-            currentBuild.description = gitDiff
+            gitDiff = sh (script: "git log ${oldGitShortCommitId}..${gitShortCommitId} --oneline --no-merges --stat --pretty='format:%C(auto)%<(15,trunc)%h %<(15,trunc)%ar %<(20,trunc)%cn  %C(Cyan)%<(100,trunc)%s%C(reset)'", returnStdout: true).trim()
+            currentBuild.description = '<span style=font-family: monospace;">' + gitDiff + '</span>'
         } else {
             currentBuild.displayName = "#${currentBuild.number}_v${kx_version}_v${kube_version}_${gitShortCommitId}"
         }
