fb168eeaaf83bdc48a974e71adbce8c9d1c7e611
Patrick Delamere <patdel76@gmail.com>
2021-10-11 15:34:33 +0000

    Debug jenkins function

--- base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
+++ base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
@@ -7,7 +7,9 @@ def setBuildEnvironment() {
     """
     sh "pwd; ls -l"
     kx_version = sh (script: "cat versions.json | ./jq -r '.kxascode'", returnStdout: true).trim()
+    println ("Current KX.AS.CODE Version: ${kx_version}")
     kube_version = sh (script: "cat versions.json | ./jq -r '.kubernetes'", returnStdout: true).trim()
+    println ("Current Kubernetes Version: ${kube_version}")
     gitShortCommitId = sh (script: "git rev-parse --short HEAD", returnStdout: true).trim()
     try {
         if ( currentBuild.number > 1 ) {
@@ -17,13 +19,14 @@ def setBuildEnvironment() {
             println(lastCompletedBuildName)
             def (oldBuildNumber, oldKxVersion, oldKubeVersion, oldGitShortCommitId) = lastCompletedBuildName.split('_')
             println(oldBuildNumber)
-            println(oldKxVersion)
+            println ("Previous KX.AS.CODE Version: ${oldKxVersion}")
             if ( oldKxVersion != "v${kx_version}" ) {
                 println("KX.AS.CODE version has changed since the last build, ${oldKxVersion} --> v${kx_version}")
             } else {
                 println("KX.AS.CODE version has not changed since the last build - v${kx_version}")
             }
             println(oldKubeVersion)
+            println ("Previous Kubernetes Version: ${oldKubeVersion}")
             if (oldKubeVersion !=  "v${kube_version}") {
                 println("Kube version has changed since the last build, ${oldKubeVersion} --> v${kube_version}")
             } else {
