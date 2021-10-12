c4c4da0c5bad9d4d3536ff8289dc1d47c4dc7632
Patrick Delamere <patdel76@gmail.com>
2021-10-12 08:48:08 +0000

    Minor fix to HTML output for job description

--- base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
+++ base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
@@ -26,7 +26,7 @@ def setBuildEnvironment() {
             gitDiff = sh (script: """
                 header=\$(echo '<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><body style="font-family:monospace,Courier;font-size:12px">')
                 gitLog=\$(git log ${oldGitShortCommitId}..${gitShortCommitId} --oneline --no-merges --stat --pretty='format:<a style="color: orange" href="${git_source_url}/commit/%h">%h%<(15,trunc)</a> ### %<(15,trunc)%ar ### %<(20,trunc)%cn ### <span style="color: green"> %<(100,trunc)%s </span>' | sed 's/<br>/\\&lt\\;br\\&gt\\;/g' | sed 's/<p>/\\&lt\\;p\\&gt\\;/g' | rev | sed -e ':b; /###/! s/^\\([^|]*\\)*\\-/\\1§/; tb;' | rev | sed 's/§/<span style="color: red">-<\\/span>/g' | rev | sed -e ':b; /###/! s/^\\([^|]*\\)*+/\\1§/; tb;' | rev | sed 's/\$/<br>/g' | sed 's/§/<span style="color: green">-<\\/span>/g' | sed 's/###/|/g')
-                echo "${header} ${gitLog}"
+                echo "\${header} \${gitLog}"
             """, returnStdout: true).trim()
             currentBuild.description = gitDiff
         } else {
