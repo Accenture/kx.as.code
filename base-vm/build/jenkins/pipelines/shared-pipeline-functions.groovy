fe266d3427f83800a692d7996a698896ad7b0cd2
Patrick Delamere <patdel76@gmail.com>
2021-10-12 06:04:38 +0000

    HTML formatted git log output

--- base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
+++ base-vm/build/jenkins/pipelines/shared-pipeline-functions.groovy
@@ -23,7 +23,12 @@ def setBuildEnvironment() {
             } else {
                 println("Kube version has not changed since the last build - v${kube_version}")
             }
-            gitDiff = sh (script: "git log ${oldGitShortCommitId}..${gitShortCommitId} --oneline --no-merges --stat --pretty='format:%C(auto)%<(15,trunc)%h %<(15,trunc)%ar %<(20,trunc)%cn  %C(Cyan)%<(100,trunc)%s%C(reset)'  | sed 's/\$/<br>/g'", returnStdout: true).trim()
+            gitDiff = sh (script: '''
+                header=\$(echo '<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><style> table { font-family: mono; font-size: 10px; border-style: solid;  border-width: 0px; border-color: #8ebf42; background-color: #ffffff;} tr { padding-bottom: 1em; solid black;} td {  vertical-align: top; text-align: left; padding: 3px; border:0px solid #1c87c9; </style><body><table>')
+                gitLog=\$(git log -n 10 --oneline --no-merges --stat --pretty='format:%C(auto)%<(15,trunc)%h ### %<(15,trunc)%ar ### %<(20,trunc)%cn ###  %C(Cyan)%<(100,trunc)%s%C(reset)' | sed 's/</\\&lt\\;/g' | sed 's/>/\\&gt\\;/g' | rev | sed -e ':b; /###/! s/^\\([^|]*\\)*\\-/\\1§/; tb;' | rev | sed 's/§/<span style="color: red">-<\\/span>/g' | rev | sed -e ':b; /###/! s/^\\([^|]*\\)*+/\\1§/; tb;' | rev | sed 's/§/<span style="color: green">-<\\/span>/g' | sed '/###/! s/^/<td colspan="4">/g' |  sed '/###/ s/^/<tr><td style="background-color: lightgrey; color: blue">/g' | sed 's/###/<\\/td><td style="background-color: lightgrey; color: blue">/g' | sed 's/$/<\\/td><\\/tr>/g')  
+                footer=\$(echo "</table></body></html>")  
+                echo "\${header} \${gitLog} \${footer}"
+            ''', returnStdout: true).trim()
             currentBuild.description = '<span style=font-family: monospace;">' + gitDiff + '</span>'
         } else {
             currentBuild.displayName = "#${currentBuild.number}_v${kx_version}_v${kube_version}_${gitShortCommitId}"
