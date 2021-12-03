5c414d4c91900299a1e7dd52a73c1898098b6723
Burak Kayaalp <burak.kayaalp@accenture.com>
2021-12-03 03:11:29 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import './App.css';
+import React, { useEffect } from 'react';
+import Dashboard from './components/Dashboard';
+import {
+  Switch,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import { focusHandling } from 'cruip-js-toolkit';
+
+import "./css/style.scss"
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+    focusHandling('outline');
+  }, [location.pathname]); // triggered on route change
+
   return (
-    <div>lorem</div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
