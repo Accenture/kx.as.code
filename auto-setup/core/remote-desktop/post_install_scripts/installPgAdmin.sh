16d2422e26b394e94889e35caa6ec316af6e2944
Patrick Delamere <patdel76@gmail.com>
2022-03-13 17:33:27 +0000

    Fixed automated pgadmin install

--- auto-setup/core/remote-desktop/post_install_scripts/installPgAdmin.sh
+++ auto-setup/core/remote-desktop/post_install_scripts/installPgAdmin.sh
@@ -7,6 +7,8 @@ export PGADMIN_DEFAULT_PASSWORD=${vmPassword}
 export PGADMIN_SETUP_EMAIL=${PGADMIN_DEFAULT_EMAIL}
 export PGADMIN_SETUP_PASSWORD=${PGADMIN_DEFAULT_PASSWORD}
 
+export PGADMIN_PLATFORM_TYPE=debian
+
 log_debug "Set PGADMIN_DEFAULT_EMAIL to ${PGADMIN_DEFAULT_EMAIL}"
 log_debug "Set PGADMIN_DEFAULT_PASSWORD to ${PGADMIN_DEFAULT_PASSWORD}"
 log_debug "Set PGADMIN_SETUP_EMAIL to ${PGADMIN_DEFAULT_EMAIL}"
@@ -27,8 +29,24 @@ log_debug "Set PGADMIN_SETUP_PASSWORD to ${PGADMIN_SETUP_PASSWORD}"
 # Install for web mode only:
 /usr/bin/sudo apt install -y pgadmin4-web
 
-# Initial setup of padmin web based administration application
-timeout -s TERM 300 /usr/bin/sudo bash -c "export PGADMIN_SETUP_EMAIL=${PGADMIN_DEFAULT_EMAIL}; export PGADMIN_SETUP_PASSWORD=${PGADMIN_DEFAULT_PASSWORD}; echo \"PGADMIN_SETUP_EMAIL=\${PGADMIN_SETUP_EMAIL}, PGADMIN_SETUP_PASSWORD=\${PGADMIN_SETUP_PASSWORD}\" && . /usr/pgadmin4/bin/setup-web.sh --yes"
+# Initial setup of pgsadmin web based administration application
+. /usr/pgadmin4/bin/setup-web.sh --yes
+
+# Temporary workaround to prevent later failures
+# TODO: Find a better solution in future. Check again whether Apache can be removed without breaking something
+if [[ -f /etc/apache2/ports.conf ]]; then
+  if [[ -z $(grep "8081" /etc/apache2/sites-available/000-default.conf) ]]; then
+      sed -i 's/:80/:8081/g' /etc/apache2/sites-available/000-default.conf
+  fi
+  if [[ -z $(grep "8081" /etc/apache2/ports.conf) ]]; then
+      sed -i 's/Listen 80/Listen 8081/g' /etc/apache2/ports.conf
+  fi
+  if [[ -z $(grep "4481" /etc/apache2/ports.conf) ]]; then
+      sed -i 's/Listen 443/Listen 4481/g' /etc/apache2/ports.conf
+  fi
+  systemctl restart apache2
+  systemctl status apache2.service
+fi
 
 # Add PGADMIN config to NGINX
 echo '''
@@ -50,9 +68,8 @@ server {
         }
 
         location /pgadmin4 {
-        proxy_pass http://127.0.0.1:8081/pgadmin4/;
+            proxy_pass http://127.0.0.1:8081/pgadmin4/;
         }
-
 }
 ''' | /usr/bin/sudo tee /etc/nginx/sites-available/pgadmin.conf
 
