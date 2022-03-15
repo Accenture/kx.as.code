9ce385d10230e39fb90e8387699203407ad0d65c
Patrick Delamere <patdel76@gmail.com>
2022-03-15 15:21:45 +0000

    Minor update to new insecure git clone error

--- base-vm/scripts/common/powerline.sh
+++ base-vm/scripts/common/powerline.sh
@@ -43,7 +43,7 @@ sudo -H -i -u $VM_USER sh -c 'cd fonts && ./install.sh && cd .. && rm -rf fonts'
 sudo chown -hR $VM_USER:$VM_USER /home/$VM_USER
 
 # System level updates
-sudo pip3 install git+git://github.com/Lokaltog/powerline
+sudo pip3 install git+https://github.com/Lokaltog/powerline
 sudo wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 sudo wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 sudo mv -f PowerlineSymbols.otf /usr/share/fonts/
