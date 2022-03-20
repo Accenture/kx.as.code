dd56515c30ef37445ee222fc0b1fe9f07fd0303e
Patrick Delamere <patdel76@gmail.com>
2022-03-20 20:48:03 +0000

    Multiple changes to Gitlab and Mattermost

--- auto-setup/collaboration/confluence/pre_install_scripts/createGitProject.sh
+++ auto-setup/collaboration/confluence/pre_install_scripts/createGitProject.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 set -euo pipefail
 
-. ${autoSetupHome}/cicd/gitlab-ce/helper_scripts/getGroupIds.sh
+. ${autoSetupHome}/cicd/gitlab/helper_scripts/getGroupIds.sh
 
 # Create Confluence project in Gitlab
 export confluenceProjectId=$(curl -s --header "Private-Token: ${personalAccessToken}" ${gitUrl}/api/v4/projects | jq '.[] | select(.name=="confluence") | .id')
