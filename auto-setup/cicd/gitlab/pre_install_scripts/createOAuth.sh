ea95c38edd6a0fb7f5350f9fbb182be2d32f08a9
Patrick Delamere <patdel76@gmail.com>
2022-05-26 17:13:07 +0100

    Handle Keycloak not installed

--- auto-setup/cicd/gitlab/pre_install_scripts/createOAuth.sh
+++ auto-setup/cicd/gitlab/pre_install_scripts/createOAuth.sh
@@ -11,31 +11,35 @@ export clientSecret=$(getKeycloakClientSecret "${clientId}")
 
 ################### Kubernetes manifests CRUD operations #####################################
 
-echo '''
-{
-  "name": "openid_connect",
-  "label": "Keycloak",
-  "args": {
+if [[ -n ${clientId} ]] && [[ -n ${clientSecret} ]]; then
+
+  echo '''
+  {
     "name": "openid_connect",
-    "scope": ["openid","profile","email"],
-    "response_type": "code",
-    "issuer": "https://keycloak.'${baseDomain}'/auth/realms/'${baseDomain}'",
-    "discovery": true,
-    "client_auth_method": "query",
-    "send_scope_to_token_endpoint": false,
-    "user_response_structure": {
-      "id_path": "preferred_username",
-      "attributes": {  "nickname":  "preferred_username"  }
-    },
-    "client_options": {
-      "identifier": "'${componentName}'",
-      "secret": "'${clientSecret}'",
-      "redirect_uri": "https://'${componentName}'.'${baseDomain}'/users/auth/openid_connect/callback"
+    "label": "Keycloak",
+    "args": {
+      "name": "openid_connect",
+      "scope": ["openid","profile","email"],
+      "response_type": "code",
+      "issuer": "https://keycloak.'${baseDomain}'/auth/realms/'${baseDomain}'",
+      "discovery": true,
+      "client_auth_method": "query",
+      "send_scope_to_token_endpoint": false,
+      "user_response_structure": {
+        "id_path": "preferred_username",
+        "attributes": {  "nickname":  "preferred_username"  }
+      },
+      "client_options": {
+        "identifier": "'${componentName}'",
+        "secret": "'${clientSecret}'",
+        "redirect_uri": "https://'${componentName}'.'${baseDomain}'/users/auth/openid_connect/callback"
+      }
     }
   }
-}
-''' | /usr/bin/sudo tee ${installationWorkspace}/gitlab-sso-providers.yaml
+  ''' | /usr/bin/sudo tee ${installationWorkspace}/gitlab-sso-providers.yaml
 
-# Check if SSO provider already exists, else create it
-kubectl get secret sso-provider --namespace=${namespace} || \
-  kubectl create secret generic sso-provider --namespace=${namespace} --from-file=provider=${installationWorkspace}/gitlab-sso-providers.yaml
+  # Check if SSO provider already exists, else create it
+  kubectl get secret sso-provider --namespace=${namespace} || \
+    kubectl create secret generic sso-provider --namespace=${namespace} --from-file=provider=${installationWorkspace}/gitlab-sso-providers.yaml
+
+fi
