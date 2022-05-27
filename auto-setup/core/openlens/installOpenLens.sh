88642611c90ff9fdbe73d419da5357585cfe8041
Patrick Delamere <patdel76@gmail.com>
2022-05-27 23:29:35 +0100

    Changed kontena-lens to openlens

--- auto-setup/core/openlens/installOpenLens.sh
+++ auto-setup/core/openlens/installOpenLens.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 /usr/bin/sudo mkdir -p /home/${vmUser}/.config/OpenLens
 
+runningKubeVersion=$(kubectl version -o json | jq -r '.serverVersion | .gitVersion')
+
 echo '''
 {
 	"__internal__": {
@@ -20,7 +22,7 @@ echo '''
 				"clusterName": "kubernetes-admin@kubernetes"
 			},
 			"metadata": {
-				"version": "'${kubeVersion}'",
+				"version": "'${runningKubeVersion}'",
 				"prometheus": {
 					"autoDetected": true,
 					"success": false
@@ -30,23 +32,6 @@ echo '''
 		}
 	]
 }''' | /usr/bin/sudo tee /home/${vmUser}/.config/OpenLens/lens-cluster-store.json
-
-echo '''
-{
-	"workspaces": [
-		{
-			"id": "default",
-			"name": "default",
-			"activeClusterId": "1374438c-6a87-4cc7-8675-1fd3ecb4829f"
-		}
-	],
-	"__internal__": {
-		"migrations": {
-			"version": "'${lensVersion}'"
-		}
-	},
-	"currentWorkspace": "default"
-}''' | /usr/bin/sudo tee /home/${vmUser}/.config/OpenLens/lens-workspace-store.json
 
 /usr/bin/sudo chown -R ${vmUser}:${vmUser} /home/${vmUser}/.config/OpenLens
 
@@ -84,9 +69,9 @@ Comment=Lens - The Kubernetes IDE
 Exec=/opt/OpenLens/open-lens %U
 GenericName[en_US]=
 GenericName=
-Icon=lens
+Icon=open-lens
 MimeType=
-Name=Lens\nKubernetes IDE
+Name=OpenLens\nKubernetes IDE
 Path=
 StartupNotify=true
 StartupWMClass=Lens
@@ -97,6 +82,6 @@ X-DBUS-ServiceName=
 X-DBUS-StartupType=
 X-KDE-SubstituteUID=false
 X-KDE-Username=
-''' | /usr/bin/sudo tee /home/${vmUser}/Desktop/kontena-lens.desktop
-/usr/bin/sudo chmod 755 /home/${vmUser}/Desktop/kontena-lens.desktop
-/usr/bin/sudo chown ${vmUser}:${vmUser} /home/${vmUser}/Desktop/kontena-lens.desktop
+''' | /usr/bin/sudo tee /home/${vmUser}/Desktop/openlens.desktop
+/usr/bin/sudo chmod 755 /home/${vmUser}/Desktop/openlens.desktop
+/usr/bin/sudo chown ${vmUser}:${vmUser} /home/${vmUser}/Desktop/openlens.desktop
