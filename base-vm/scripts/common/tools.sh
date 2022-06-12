ff157be11df9279965580992e3c8b139a38c8432
Patrick Delamere <patdel76@gmail.com>
2022-06-12 19:46:48 +0100

    Changed HTOP to BTOP

--- base-vm/scripts/common/tools.sh
+++ base-vm/scripts/common/tools.sh
@@ -15,7 +15,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install \
     git \
     jq \
     htop \
-    btop \
     sshpass \
     fontconfig \
     fontconfig-config \
@@ -44,3 +43,13 @@ sudo pip3 install powerline-status
 
 # Install Netplan for later NIC configuration on first start
 sudo apt-get install -y netplan.io
+
+# Install BTOP
+mkdir ${INSTALLATION_WORKSPACE}/btop
+cd ${INSTALLATION_WORKSPACE}/btop
+wget https://github.com/aristocratos/btop/releases/download/v1.2.7/btop-x86_64-linux-musl.tbz
+echo "b3b7cd2a8ef6ebbbadab3ca7689096efa3e4b1e4e11b9a68ed6dedb1e3475fb5 btop-x86_64-linux-musl.tbz" | sha256sum --check
+bunzip2 ${INSTALLATION_WORKSPACE}/btop/btop-x86_64-linux-musl.tbz
+tar xvf ${INSTALLATION_WORKSPACE}/btop/btop-x86_64-linux-musl.tar
+sudo cp -f ${INSTALLATION_WORKSPACE}/btop/bin/btop /usr/local/bin
+rm -rf ${INSTALLATION_WORKSPACE}/btop
--- base-vm/scripts/main-node/profile.sh
+++ base-vm/scripts/main-node/profile.sh
@@ -7,8 +7,8 @@ sudo cp -rfT ${INSTALLATION_WORKSPACE}/skel /root
 sudo chown -R ${VM_USER}:${VM_USER} /home/${VM_USER}
 sudo chmod -R 755 /home/${VM_USER}/Desktop/*.desktop
 
-# Update system desktop icon file for HTOP
-sudo cp -f /home/${VM_USER}/Desktop/HTOP.desktop /usr/share/applications/htop.desktop
+# Update system desktop icon file for BTOP
+sudo cp -f /home/${VM_USER}/Desktop/BTOP.desktop /usr/share/applications/btop.desktop
 sudo cp -f /home/${VM_USER}/Desktop/Tilix.desktop /usr/share/applications/com.gexperts.Tilix.desktop
 
 # Change screen resolution to more respectable 1920x1200 (default is 800x600)
