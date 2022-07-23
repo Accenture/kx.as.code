55140adfbdf2957d25abd1a679910ad5c967893d
Patrick Delamere <patdel76@gmail.com>
2022-07-23 23:26:57 +0100

    Packer Common scripts updated for ARM64

--- base-vm/scripts/common/calico.sh
+++ base-vm/scripts/common/calico.sh
@@ -2,9 +2,23 @@
 set -euo pipefail
 
 # Install calicoctl
-curl -O -L  https://github.com/projectcalico/calicoctl/releases/download/v3.19.1/calicoctl
-sudo chmod +x calicoctl
-sudo mv calicoctl /usr/local/bin
+mkdir ${INSTALLATION_WORKSPACE}/calico
+cd ${INSTALLATION_WORKSPACE}/calico
+if [[ -n $( uname -a | grep "aarch64") ]]; then
+  # Download URL for ARM64 CPU architecture
+  CALICOCTL_URL="https://github.com/projectcalico/calicoctl/releases/download/v3.21.5/calicoctl-linux-arm64"
+  CALICOCTL_CHECKSUM="cc73e2b8f5b695b6ab06e7856cd516c1e9ec3e903abb510ef465ca6b530e18e6"
+else
+  # Download URL for X86_64 CPU architecture
+  CALICOCTL_URL="https://github.com/projectcalico/calicoctl/releases/download/v3.21.5/calicoctl-linux-amd64"
+  CALICOCTL_CHECKSUM="98407b1c608fec0896004767c72cd4b6cf939976d67d3eca121f1f02137c92a7"
+fi
+
+wget  ${CALICOCTL_URL}
+CALICOCTL_CHECKSUM=$(basename ${CALICOCTL_URL})
+echo "${CALICOCTL_CHECKSUM} ${CALICOCTL_FILE}" | sha256sum --check
+sudo chmod +x ${CALICOCTL_FILE}
+sudo mv ${CALICOCTL_FILE} /usr/local/bin/calicoctl
 
 # Ensure Network-Manager does not interfere with Calico
 echo """
--- base-vm/scripts/common/kx.as.code.sh
+++ base-vm/scripts/common/kx.as.code.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -x
 set -euo pipefail
 
-echo "COMPUTE_ENGINE_BUILD: ${COMPUTE_ENGINE_BUILD}"
+if [[ -z $( uname -a | grep "aarch64") ]]; then
 
-if [[ ${COMPUTE_ENGINE_BUILD} == "true"   ]]; then
-    # Remove splash screen from bootloader for cloud builds
-    sudo sed -E -i 's/GRUB_CMDLINE_LINUX_DEFAULT="(.+)"/GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS0,38400n8d"/g' /etc/default/grub
-    sudo update-grub
-else
-    # Ensure KX.AS.CODE splash screen is shown during boot-up
-    sudo sed -E -i 's/GRUB_CMDLINE_LINUX_DEFAULT="(.+)"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
-    sudo update-grub
+  echo "COMPUTE_ENGINE_BUILD: ${COMPUTE_ENGINE_BUILD}"
+  if [[ ${COMPUTE_ENGINE_BUILD} == "true"   ]]; then
+      # Remove splash screen from bootloader for cloud builds
+      sudo sed -E -i 's/GRUB_CMDLINE_LINUX_DEFAULT="(.+)"/GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS0,38400n8d"/g' /etc/default/grub
+      sudo update-grub
+  else
+      # Ensure KX.AS.CODE splash screen is shown during boot-up
+      sudo sed -E -i 's/GRUB_CMDLINE_LINUX_DEFAULT="(.+)"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
+      sudo update-grub
 
-    # Customize boot splash screen
-    sudo apt-get install -y plymouth-themes
-    sudo cp -iRv ${INSTALLATION_WORKSPACE}/theme/plymouth/kx.as.code /usr/share/plymouth/themes/
-    sudo plymouth-set-default-theme -R kx.as.code
+      # Customize boot splash screen
+      sudo apt-get install -y plymouth-themes
+      sudo cp -iRv ${INSTALLATION_WORKSPACE}/theme/plymouth/kx.as.code /usr/share/plymouth/themes/
+      sudo plymouth-set-default-theme -R kx.as.code
+  fi
+  cat /etc/default/grub
+
 fi
-
-cat /etc/default/grub
