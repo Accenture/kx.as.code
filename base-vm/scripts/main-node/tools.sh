10d513cc782363107a93a0a96bd8c22f8a10b1c0
Patrick Delamere <patdel76@gmail.com>
2022-07-24 12:27:49 +0100

    Updated default browser to chromium for ARM64 - Raspberry Pi

--- base-vm/scripts/main-node/tools.sh
+++ base-vm/scripts/main-node/tools.sh
@@ -28,7 +28,7 @@ sudo apt-get -y install \
 
 # Install Google-Chrome
 if [[ "${ARCH}" == "arm64" ]]; then
-  sudo apt install -y chromium-browser
+  sudo apt install -y chromium
 else
   wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
   sudo sh -c 'echo "deb [arch='${ARCH}'] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
