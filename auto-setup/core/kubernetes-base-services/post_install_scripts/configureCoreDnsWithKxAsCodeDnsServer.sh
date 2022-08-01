b469adc448e8df0887d88fef2610ad42aea36b30
Patrick Delamere <patdel76@gmail.com>
2022-08-01 18:58:29 +0100

    Minor update to Kubernetes coredns configuration script

--- auto-setup/core/kubernetes-base-services/post_install_scripts/configureCoreDnsWithKxAsCodeDnsServer.sh
+++ auto-setup/core/kubernetes-base-services/post_install_scripts/configureCoreDnsWithKxAsCodeDnsServer.sh
@@ -7,10 +7,14 @@ set -euo pipefail
 waitForKubernetesResource "coredns" "configmap" "kube-system"
 
 # Get existing config-map
-kubectl get cm -n kube-system coredns -o yaml >${installationWorkspace}/coredns.yaml
+kubectl get cm -n kube-system coredns -o yaml | tee /usr/bin/sudo ${installationWorkspace}/coredns.yaml
 
 # Update exported YAML file with external DNS server (bind9 instance installed on KX-Main1)
-sed -i -e '/^        loadbalance/{:a; N; /\n    }/!ba; a \    '${baseDomain}':53 {\n        errors\n        cache 30\n        forward . '${mainIpAddress}'\n    }' -e '}' ${installationWorkspace}/coredns.yaml
+/usr/bin/sudo sed -i -e '/^        loadbalance/{:a; N; /\n    }/!ba; a \    '${baseDomain}':53 {\n        errors\n        cache 30\n        forward . '${mainIpAddress}'\n    }' -e '}' ${installationWorkspace}/coredns.yaml
 
-# Apply the updated config-map
-kubectl apply -f ${installationWorkspace}/coredns.yaml -n kube-system
+# Validate YAML file before applying
+kubeval ${installationWorkspace}/${yamlFilename} --schema-location https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master --strict || rc=$? && log_info "kubeval returned with rc=$rc"
+
+# Validate and apply the updated config-map
+kubernetesApplyYamlFile "${installationWorkspace}/coredns.yaml" "kube-system"
+
