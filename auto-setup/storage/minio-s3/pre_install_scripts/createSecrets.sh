892bdd593d23d37aabad62df368aa2a814d0e2c6
Patrick Delamere <patdel76@gmail.com>
2022-08-06 23:53:01 +0200

    Reduced resource requests for Minio-S3

--- auto-setup/storage/minio-s3/pre_install_scripts/createSecrets.sh
+++ auto-setup/storage/minio-s3/pre_install_scripts/createSecrets.sh
@@ -10,14 +10,3 @@ if [ -z $(kubectl get secrets -n minio-s3 --output=name --field-selector metadat
         --from-literal=rootPassword="${minio-admin-password}" \
         --namespace minio-s3
 fi
-
-# Check if secret already exists in case of re-run of this script
-if [ -z $(kubectl get secrets -n minio-s3 --output=name --field-selector metadata.name=minio-accesskey-secret) ]; then
-    # Create MinIO Access Key secret
-    export minioAccessKey=$(managedApiKey "minio-s3-access-key" "minio-s3")
-    export minioSecretKey=$(managedApiKey "minio-s3-secret-key" "minio-s3")
-    kubectl create secret generic minio-accesskey-secret \
-        --from-literal=accesskey=${minioAccessKey} \
-        --from-literal=secretkey=${minioSecretKey} \
-        --namespace minio-s3
-fi
