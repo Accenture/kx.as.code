4483ccd292c76f99f6856ee3f4cb252104ce7b2a
Patrick Delamere <patdel76@gmail.com>
2022-08-10 11:33:12 +0000

    Minor fix to krew install script

--- auto-setup/core/kubernetes-tools/post_install_scripts/installPluginManager.sh
+++ auto-setup/core/kubernetes-tools/post_install_scripts/installPluginManager.sh
@@ -7,10 +7,10 @@ declare krewChecksum="krew${cpuArchitecture^}Checksum"
 # Download and install Krew
 downloadFile "https://github.com/kubernetes-sigs/krew/releases/download/${krewVersion}/krew-linux_${cpuArchitecture}.tar.gz" \
   "${!krewChecksum}" \
-  "${installationWorkspace}/${krewVersion}/krew-linux_${cpuArchitecture}.tar.gz"
+  "${installationWorkspace}/krew-linux_${cpuArchitecture}.tar.gz"
 
 # Unpack downloaded file
-tar xvzf ${installationWorkspace}/${krewVersion}/krew-linux_${cpuArchitecture}.tar.gz --directory ${installationWorkspace}
+tar xvzf ${installationWorkspace}/krew-linux_${cpuArchitecture}.tar.gz --directory ${installationWorkspace}
 
 # Put Krew on the path before calling it
 /usr/bin/sudo cp ${installationWorkspace}/krew-linux_${cpuArchitecture} /usr/local/bin/krew
@@ -20,10 +20,10 @@ tar xvzf ${installationWorkspace}/${krewVersion}/krew-linux_${cpuArchitecture}.t
 /usr/bin/sudo cp -f /root/.krew/bin/kubectl-krew /usr/local/bin
 
 # Update PATH
-if [[ -z $(grep "KREW_ROOT" /root/.zshrc) ]]
+if [[ -z $(grep "KREW_ROOT" /root/.zshrc) ]]; then
   echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' | /usr/bin/sudo tee -a /root/.zshrc /root/.bashrc
 fi
 
-if [[ -z $(grep "KREW_ROOT" /home/${baseUser}/.bashrc) ]]
+if [[ -z $(grep "KREW_ROOT" /home/${baseUser}/.bashrc) ]]; then
   /usr/bin/sudo -H -i -u ${baseUser} bash -c "echo 'export PATH=\"\\\${KREW_ROOT:-\\\$HOME/.krew}/bin:\\\$PATH\"' | /usr/bin/sudo tee -a /home/${baseUser}/.zshrc /home/${baseUser}/.bashrc"
 fi
