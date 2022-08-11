3acec0e4929ac7dc76ed1faf1c2509ef36c94837
Patrick Delamere <patdel76@gmail.com>
2022-08-11 10:10:25 +0100

    Minor update to MinIO Operator

--- auto-setup/functions/minioS3GetAccessAndSecretKeys.sh
+++ auto-setup/functions/minioS3GetAccessAndSecretKeys.sh
@@ -8,8 +8,8 @@ minioS3GetAccessAndSecretKeys() {
         username=${1}
 
         # Get MinIO Access Access and Secret Keys
-        export minioAccessKey=$(managedApiKey "minio-s3-${username}-sa-access-key" "minio-s3")
-        export minioSecretKey=$(managedApiKey "minio-s3-${username}-sa-secret-key" "minio-s3")
+        export minioAccessKey=$(managedApiKey "minio-s3-${username}-sa-access-key" "minio-operator")
+        export minioSecretKey=$(managedApiKey "minio-s3-${username}-sa-secret-key" "minio-operator")
         
     fi
 
