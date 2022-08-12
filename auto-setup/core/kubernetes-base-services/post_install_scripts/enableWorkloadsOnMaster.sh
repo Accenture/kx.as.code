c2d500c1fce1c5163e5dc04264abfb8060768815
Patrick Delamere <patdel76@gmail.com>
2022-08-12 08:40:17 +0000

    Updated script to remove node taints

--- auto-setup/core/kubernetes-base-services/post_install_scripts/enableWorkloadsOnMaster.sh
+++ auto-setup/core/kubernetes-base-services/post_install_scripts/enableWorkloadsOnMaster.sh
@@ -8,10 +8,10 @@ if [[ ${allowWorkloadsOnMaster} == "true"   ]]; then
     # Checking if taint already removed
     controlPlaneNodeTaints=$(sudo kubectl get nodes --selector=node-role.kubernetes.io/control-plane -o json | jq '.items[]?.spec.taints | select(.[]?.key=="node-role.kubernetes.io/control-plane")')
     if [[ -n ${controlPlaneNodeTaints} ]]; then
-      kubectl taint nodes --selector=node-role.kubernetes.io/master node-role.kubernetes.io/control-plane=:NoSchedule- --overwrite=true
+      kubectl taint nodes --selector=node-role.kubernetes.io/control-plane node-role.kubernetes.io/control-plane=:NoSchedule- --overwrite=true
     fi
     masterNodeTaints=$(sudo kubectl get nodes --selector=node-role.kubernetes.io/master -o json | jq '.items[]?.spec.taints | select(.[]?.key=="node-role.kubernetes.io/master")')
     if [[ -n ${masterNodeTaints} ]]; then
-      kubectl taint nodes --selector=node-role.kubernetes.io/master node-role.kubernetes.io/master=:NoSchedule- --overwrite=true
+      kubectl taint nodes --selector=node-role.kubernetes.io/control-plane node-role.kubernetes.io/master=:NoSchedule- --overwrite=true
     fi
 fi
