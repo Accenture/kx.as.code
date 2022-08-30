b60e919aa4beab2ba8c42535aedc19585f06f1b7
Patrick Delamere <patdel76@gmail.com>
2022-08-30 15:45:58 +0200

    [#323] Minor fix to addToNotificationQueue.sh

--- auto-setup/functions/addToNotificationQueue.sh
+++ auto-setup/functions/addToNotificationQueue.sh
@@ -22,7 +22,7 @@ addToNotificationQueue() {
   export notificationPayload='{ "message": "'${message}'", "application": "'${componentName}'", "installation_folder": "'${componentInstallationFolder}'", "message_type": "action_status", "action": "'${action}'", "action_status": "'${action_status}'", "log_level": "'${log_level}'", "timestamp": "'$(date +%s)'" }'
   log_debug export notificationPayload='{ "message": "'${message}'", "application": "'${componentName}'", "installation_folder": "'${componentInstallationFolder}'", "message_type": "action_status", "action": "'${action}'", "action_status": "'${action_status}'", "log_level": "'${log_level}'", "timestamp": "'$(date +%s)'" }'
   log_debug rabbitmqadmin publish exchange=action_workflow routing_key=notification_queue properties='{"delivery_mode": 2}' notificationPayload="${notificationPayload}"
-  rabbitmqadmin publish exchange=action_workflow routing_key=notification_queue properties='{"delivery_mode": 2}' notificationPayload="${notificationPayload}"
+  rabbitmqadmin publish exchange=action_workflow routing_key=notification_queue properties='{"delivery_mode": 2}' payload="${notificationPayload}"
 
   # Call common function to execute common function start commands, such as unsetting verbose output etc
   functionEnd
