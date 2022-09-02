21aaef6cbbded31643cd3b355c8aa7228c86196e
Burak Kayaalp <burak.kayaalp@accenture.com>
2022-09-02 22:32:13 +0200

    add new app group components sextion in app grouop cards with interactive galery, slider, back and next buttons, component name on hover, forward to each application details page on click, etc.

--- client/src/partials/applicationGroups/ApplicationGroupCard.jsx
+++ client/src/partials/applicationGroups/ApplicationGroupCard.jsx
@@ -1,31 +1,40 @@
 import React, { useState, useEffect, Component } from "react";
-import { Link } from "react-router-dom";
-import EditMenu from "../EditMenu";
-import { TrashCan32, Restart32 } from "@carbon/icons-react";
+import { Link, NavLink } from "react-router-dom";
+// import EditMenu from "../EditMenu";
+// import { TrashCan32, Restart32 } from "@carbon/icons-react";
 import ApplicationStatusActionButton from "../applications/ApplicationStatusActionButton";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import Tooltip from "@mui/material/Tooltip";
 
-import image from "../../media/png/appImgs/postman.png";
+// import image from "../../media/png/appImgs/postman.png";
 
-import AliceCarousel from "react-alice-carousel";
+// import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
-const responsive = {
-  0: { items: 3 },
-  568: { items: 3 },
-  1024: { items: 3 },
-};
-const Gallery = (props) => {
-  return (
-    <AliceCarousel
-      mouseTracking
-      items={props.itemsList}
-      responsive={responsive}
-      controlsStrategy="default"
-      disableDotsControls={true}
-      // paddingRight={20}
-    />
-  );
-};
+// const responsive = {
+//   0: { items: 3 },
+//   568: { items: 3 },
+//   1024: { items: 3 },
+// };
+// const Gallery = (props) => {
+//   return (
+//     <AliceCarousel
+//       mouseTracking
+//       items={props.itemsList}
+//       responsive={responsive}
+//       controlsStrategy="default"
+//       disableDotsControls={true}
+//       // paddingRight={20}
+//     />
+//   );
+// };
 
 const handleDragStart = (e) => e.preventDefault();
 
@@ -49,35 +58,17 @@ function ApplicationGroupCard(props) {
       return q.name;
     });
     setImageList(components);
+    setAppGroupComponents(components);
   }
 
   const setImageList = (appGroupComponents) => {
     // var itemsTmp = [];
     appGroupComponents.map((appName) => {
-      console.log("appName setImg: ", appName);
-      console.log("itemsList: ", itemsList);
-
-      // const res = import(`../../media/png/appImgs/${appName}.png`);
-      // console.log("RES: ", res);
-
-      // setItemsList(
-
-      // )
-      //   <img
-      //     src={require(`../../media/png/appImgs/${appName}.png`).default}
-      //     onDragStart={handleDragStart}
-      //     role="presentation"
-      //   />
-      // );
-
-      // setItemsList(itemsTmp);
-
       setItemsList((current) => [
         ...current,
         <img
-          className="bg-ghBlack3 p-3 rounded"
-          heigth="50px"
-          width="50px"
+          className="p-1 rounded"
+          style={{ height: "50px", width: "50px" }}
           src={require(`../../media/png/appImgs/${appName}.png`).default}
           onDragStart={handleDragStart}
           role="presentation"
@@ -131,14 +122,51 @@ function ApplicationGroupCard(props) {
 
           {/* Main Card Content */}
           {/* <div className="mb-4">{props.appGroup.description}</div> */}
-
+          {/* 
           <div className="flex">
             <ul className="float-left">
               {drawApplicationGroupCardComponentsTags(appGroupComponents)}
             </ul>
-          </div>
-          <div>
-            <Gallery itemsList={itemsList} />
+          </div> */}
+          <div className="">
+            {/* <Gallery itemsList={itemsList} /> */}
+
+            <CarouselProvider
+              visibleSlides={4}
+              totalSlides={6}
+              step={3}
+              naturalSlideWidth={400}
+              naturalSlideHeight={400}
+            >
+              <Slider>
+                {itemsList.map((image, i) => {
+                  return (
+                    <Slide index={i}>
+                      <Tooltip
+                        title={appGroupComponents[i]}
+                        placement="top"
+                        arrow
+                      >
+                        <NavLink to={`/apps/${appGroupComponents[i]}`}>
+                          <div className="flex justify-center rounded-md hover:bg-gray-600 p-2 hover:pointer">
+                            {image}
+                          </div>
+                        </NavLink>
+                      </Tooltip>
+                    </Slide>
+                  );
+                })}
+              </Slider>
+
+              <div className="flex justify-between py-6">
+                <ButtonBack className="hover:bg-gray-600 p-2 px-3 text-sm rounded items-center flex">
+                  Back
+                </ButtonBack>
+                <ButtonNext className="hover:bg-gray-600 p-2 px-3 text-sm rounded items-center flex">
+                  Next
+                </ButtonNext>
+              </div>
+            </CarouselProvider>
           </div>
           <div className="">
             <ApplicationStatusActionButton
@@ -152,39 +180,6 @@ function ApplicationGroupCard(props) {
             />
           </div>
         </div>
-
-        {/* <div className="w-full flex justify-center">
-          <button
-            disabled
-            className="btn rounded bg-kxBlue hover:bg-kxBlue2 p-2 px-6 absolute bottom-0 mb-8 "
-          >
-            <svg
-              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-            INSTALLING
-          </button>
-        </div> */}
-
-        {/* Install Processing Bar */}
-        {/* <div className="">
-          <div className="rounded-t-none rounded bg-statusNewGreen absolute bottom-0 justify-center flex px-6 h-3 w-full"></div>
-        </div> */}
       </div>
     </div>
   );
