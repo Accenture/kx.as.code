772a8b9389985cd66d40300568c0102d093667eb
Patrick Delamere <patdel76@gmail.com>
2023-02-20 11:36:55 +0100

    [#383] Added Slack, MS-Teams and E-Mail notification mechanisms

--- auto-setup/functions/notifyAllChannels.sh
+++ auto-setup/functions/notifyAllChannels.sh
@@ -1,13 +1,29 @@
 notifyAllChannels() {
 
-  # Call common function to execute common function start commands, such as setting verbose output etc
-  functionStart
-
   local message=${1}
   local logLevel=${2-info}
   local actionStatus=${3-unknown}
   local action=${4-}
-  local notificationTimeout=${5-300000}
+  local acionQueueJsonPayload=${5-}
+  local actionDuration=${6-}
+  local notificationTimeout=${7-300000}
+
+  if [[ -n ${acionQueueJsonPayload} ]]; then
+
+    local category=$(echo ${acionQueueJsonPayload} | jq -r '.install_folder')
+    local componentName=$(echo ${acionQueueJsonPayload} | jq -r '.name')
+    local action=$(echo ${acionQueueJsonPayload} | jq -r '.action')
+    local retries=$(echo ${acionQueueJsonPayload} | jq -r '.retries')
+
+    if [[ "${action}" == "executeTask" ]]; then
+      local task=$(echo ${acionQueueJsonPayload} | jq -r '.task')
+      action="Task Execution"
+    elif [[ "${action}" == "install" ]]; then
+      local task="n/a"
+      action="Component Installation"
+    fi
+
+  fi
 
   if [[ "${logLevel}" == "error" ]]; then
     dialogType="dialog-error"
@@ -21,16 +37,18 @@ notifyAllChannels() {
   fi
 
   # Change message if task was executed rather than solution installed
-  if [[ "${action}" == "executeTask" ]]; then
-    message=$(echo "${1}" | sed 's/installation/task execution/g' | sed 's/installed/task executed/g')
+  if [[ "${action}" == "Task Execution" ]]; then
+    message=$(echo "${1}" | sed "s/Installation of/Execution of task ''${task}'' for/g" | sed "s/installed/task ''${task}'' executed/g")
   fi
 
-  log_debug notify "${message}" "${dialogType}"
+  log_debug "notify \"${message}\" \"${dialogType}\""
+
+  sendSlackNotification "${message}" "${logLevel}" "${actionStatus}" "${componentName:-}" "${action}" "${category:-}" "${retries:-}" "${task:-}" "${autoSetupDuration:-}"
+  sendMsTeamsNotification "${message}" "${logLevel}" "${actionStatus}" "${componentName:-}" "${action}" "${category:-}" "${retries:-}" "${task:-}" "${autoSetupDuration:-}"
+  sendEmailNotification "${message}" "${logLevel}" "${actionStatus}" "${componentName:-}" "${action}" "${category:-}" "${retries:-}" "${task:-}" "${autoSetupDuration:-}"
+
   notify "${message}" "${dialogType}" "${notificationTimeout}"
   log_debug addToNotificationQueue "${message}" "${logLevel}" "${actionStatus}"
   addToNotificationQueue "${message}" "${logLevel}" "${actionStatus}"
-
-  # Call common function to execute common function start commands, such as unsetting verbose output etc
-  functionEnd
 
 }
