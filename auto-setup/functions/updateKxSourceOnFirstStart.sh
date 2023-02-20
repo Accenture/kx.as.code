623a55028e8554c5505a425dde612b287a2a3154
Patrick Delamere <patdel76@gmail.com>
2023-02-20 12:02:44 +0100

    [#395] Enabled automated framework update also when credentials are required

--- auto-setup/functions/updateKxSourceOnFirstStart.sh
+++ auto-setup/functions/updateKxSourceOnFirstStart.sh
@@ -5,11 +5,27 @@ updateKxSourceOnFirstStart() {
 
   if [[ "${updateSourceOnStart}" == "true" ]] && [[ ! -f /usr/share/kx.as.code/.config/network_status ]]; then
 
-   # Pull latest source code from Github.com
+  # Ensure no Windows characters blocking decryption
+  /usr/bin/sudo apt-get install dos2unix
+  /usr/bin/sudo dos2unix ${sharedKxHome}/.config/.vmCredentialsFile
+
+  # Get git credentials
+  local hash="$(/usr/bin/sudo cat /var/tmp/.hash)"
+  local gitUsername=$(/usr/bin/sudo cat ${sharedKxHome}/.config/.vmCredentialsFile | grep "git_source_username" | cut -f 2 -d':' | openssl enc -aes-256-cbc -pbkdf2 -salt -A -a -pass pass:${hash} -d | sed 's/@/%40/g')
+  local gitPassword=$(/usr/bin/sudo cat ${sharedKxHome}/.config/.vmCredentialsFile | grep "git_source_password" | cut -f 2 -d':' | openssl enc -aes-256-cbc -pbkdf2 -salt -A -a -pass pass:${hash} -d)
+
+   # Pull latest source code from the source code repository
    log_debug "updateSourceOnStart set to true. Pulling latest code from Github"
 
    # Defined in globalVariables.json. Value of sharedGitHome at time of writing is /usr/share/kx.as.code/git
    cd ${sharedGitHome}/kx.as.code
+
+   # Set origin to include credentials if applicable
+   gitOriginUrl=$(git config --get remote.origin.url)
+   if [[ -n ${gitUsername} ]] && [[ -n ${gitPassword} ]]; then
+     gitOriginUrlTemp=$(echo ${gitOriginUrl} | sed 's;https://;;g')
+     gitOriginUrl="https://${gitUsername}:${gitPassword}@${gitOriginUrlTemp}"
+   fi
 
    # Get current branch
    currentGitBranch=$(git branch --show-current)
@@ -18,16 +34,16 @@ updateKxSourceOnFirstStart() {
    /usr/bin/sudo git remote set-branches origin '*'
 
    # Fetch changes before pulling in order to generate a change log
-   /usr/bin/sudo git fetch -v
+   /usr/bin/sudo git fetch ${gitOriginUrl} ${currentGitBranch} -v
 
    # Generate detailed change log
-   git --no-pager diff ${currentGitBranch} origin/${currentGitBranch} | /usr/bin/sudo tee ${installationWorkspace}/gitChangeLogDetailed.log
+   git --no-pager diff ${currentGitBranch} remotes/origin/${currentGitBranch} | /usr/bin/sudo tee ${installationWorkspace}/gitChangeLogDetailed.log
 
    # Generate summary change log
-   git --no-pager diff ${currentGitBranch} origin/${currentGitBranch} --name-status | /usr/bin/sudo tee ${installationWorkspace}/gitChangeLogSummary.log
+   git --no-pager diff ${currentGitBranch} remotes/origin/${currentGitBranch} --name-status | /usr/bin/sudo tee ${installationWorkspace}/gitChangeLogSummary.log
 
    # Pull latest code from current branch. If you want to switch to another branch, you will have to navigate to the git directory and do it manually
-   /usr/bin/sudo git pull --no-edit
+   /usr/bin/sudo git pull ${gitOriginUrl} ${currentGitBranch} --no-edit
 
    # Correct permissions.
    /usr/bin/sudo chown -R ${baseUser}:${baseUser} ${sharedGitHome}
