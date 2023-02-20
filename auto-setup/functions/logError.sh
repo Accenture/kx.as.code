61398d8175ad829e5093298116cf881667ef5c45
Patrick Delamere <patdel76@gmail.com>
2023-02-20 12:22:51 +0100

    Minor updates

--- auto-setup/functions/logError.sh
+++ auto-setup/functions/logError.sh
@@ -1,5 +1,5 @@
 log_error() {
-    if [[ "${logLevel}" == "info" ]] || [[ "${logLevel}" == "error" ]] || [[ "${logLevel}" == "warn" ]]; then
-        echo "$(date '+%Y-%m-%d_%H%M%S') [ERROR] ${1}" | tee -a ${logFilename}
+    if [[ "${logLevel}" == "error" ]] || [[ "${logLevel}" == "debug" ]]; then
+        >&2 echo "$(date '+%Y-%m-%d_%H%M%S') [ERROR] ${1}" | tee -a ${logFilename}
     fi
 }
