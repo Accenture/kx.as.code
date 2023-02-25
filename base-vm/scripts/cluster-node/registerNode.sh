968ad8cec478c5c06b6caf11da5a1537ddccb728
Patrick Delamere <patdel76@gmail.com>
2023-02-25 15:27:40 +0100

    [#386] Added AWS SSM to worker nodes

--- base-vm/scripts/cluster-node/registerNode.sh
+++ base-vm/scripts/cluster-node/registerNode.sh
@@ -263,6 +263,15 @@ create_volumes $(awk "BEGIN{printf \"%.2f\", (50 * 1.02)}") ${number50gbVolumes}
 /usr/bin/sudo df -hT
 /usr/bin/sudo lsblk
 
+# Install AWS SSM Agent if running in AWS
+deployedAwsAmiImage=$(cat ${installationWorkspace}/profile-config.json | jq -r '.vm_properties.kx_main_ami_id')
+if [[ -n "${deployedAwsAmiImage}" ]] && [[ "${deployedAwsAmiImage}" != "null" ]]; then
+  mkdir ${installationWorkspace}/aws-ssm
+  curl -o ${installationWorkspace}/aws-ssm/amazon-ssm-agent.deb https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+  /usr/bin/sudo dpkg -i ${installationWorkspace}/aws-ssm/amazon-ssm-agent.deb
+  /usr/bin/sudo systemctl enable amazon-ssm-agent
+fi
+
 cd ${installationWorkspace}
 
 # Wait until the worker has the main node's IP file
