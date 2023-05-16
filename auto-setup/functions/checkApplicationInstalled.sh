64cee6ab32d126ddbbc348144bbb155311f16beb
Patrick Delamere <patdel76@gmail.com>
2023-05-16 12:21:26 +0200

    [#500] Added wrapper for manually triggering functions

--- auto-setup/functions/checkApplicationInstalled.sh
+++ auto-setup/functions/checkApplicationInstalled.sh
@@ -3,17 +3,17 @@ checkApplicationInstalled() {
   # Call common function to execute common function start commands, such as setting verbose output etc
   functionStart
 
-  export componentName=${1}
-  export componentInstallationFolder=${2}
+  export appComponentName=${1}
+  export appComponentInstallationFolder=${2}
 
   # Define component install directory
-  local installComponentDirectory=${autoSetupHome}/${componentInstallationFolder}/${componentName}
+  local installComponentDirectory=${autoSetupHome}/${appComponentInstallationFolder}/${appComponentName}
 
   # Define location of metadata JSON file for component
   local componentToCheckMetadataJson=${installComponentDirectory}/metadata.json
 
   # URL Liveliness Healthcheck
-  >&2 log_debug "Getting URL for \"${componentName}\" in \"${componentInstallationFolder}\" folder"
+  >&2 log_debug "Getting URL for \"${appComponentName}\" in \"${appComponentInstallationFolder}\" folder"
   local applicationUrl=$(cat ${componentToCheckMetadataJson} | jq -r '.urls[0]?.url?' | mo)
   local livelinessCheckData=$(cat ${componentToCheckMetadataJson} | jq -r '.urls[0]?.healthchecks?.liveliness?')
   local urlCheckPath=$(echo ${livelinessCheckData} | jq -r '.http_path')
@@ -21,11 +21,11 @@ checkApplicationInstalled() {
   # Check if application URL exists. If 404 is returned, assumption is that it is not installed
   local http_code=$(curl -s -o /dev/null -L -w '%{http_code}' ${applicationUrl}${urlCheckPath} || true)
   if [[ "${http_code}" == "404" ]]; then
-    >&2 log_debug "Application \"${componentName}\" is not installed. Will skip next steps that relies on this application to be available"
+    >&2 log_debug "Application \"${appComponentName}\" is not installed. Will skip next steps that relies on this application to be available"
     false
     return
   else
-    >&2 log_debug "Application \"${componentName}\" is installed. Continuing with application specific next steps"
+    >&2 log_debug "Application \"${appComponentName}\" is installed. Continuing with application specific next steps"
     true
     return
   fi
