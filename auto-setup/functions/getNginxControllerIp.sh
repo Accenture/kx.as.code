9a62256f69a4cfbf7efab0299c33649e7e0e2969
Patrick Delamere <patdel76@gmail.com>
2023-07-22 11:23:57 +0200

    [#535] Enhanced getNginxControllerIp() function to get external LB IP

--- auto-setup/functions/getNginxControllerIp.sh
+++ auto-setup/functions/getNginxControllerIp.sh
@@ -3,8 +3,14 @@ getNginxControllerIp() {
     # Call common function to execute common function start commands, such as setting verbose output etc
     functionStart
 
+    local ipTypeToReturn=${1:-cluster}
+
     # Get NGINX Ingress Controller IP
-    kubectl get svc nginx-ingress-controller-ingress-nginx-controller -n nginx-ingress-controller -o jsonpath={.spec.clusterIP}
+    if [[ "${ipTypeToReturn}" == "external" ]]; then
+        kubectl get svc nginx-ingress-controller-ingress-nginx-controller -n nginx-ingress-controller -o json | jq -r '.status.loadBalancer.ingress[0].ip'
+    else
+        kubectl get svc nginx-ingress-controller-ingress-nginx-controller -n nginx-ingress-controller -o jsonpath={.spec.clusterIP}
+    fi
 
     # Call common function to execute common function start commands, such as unsetting verbose output etc
     functionEnd
