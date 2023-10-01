5e060cee96d93744eee5d1e960d5a87abd1c3ccb
Patrick Delamere <patdel76@gmail.com>
2023-10-01 11:33:17 +0200

    [#569] Fixed notification functions

--- auto-setup/functions/sendEmailNotification.sh
+++ auto-setup/functions/sendEmailNotification.sh
@@ -1,29 +1,29 @@
+####_EXCLUDE_FROM_FUNCTION_HEADER_FOOTER_INJECTION_####
 sendEmailNotification() {
-
-  # Call common function to execute common function start commands, such as setting verbose output etc
-  functionStart "true"
-
-  local message=${1:-}
-  local alertType=${2:-info}
-  local actionStatus=${3:-unkown}
-  local componentName=${4:-not applicable}
+  set +x
+  local message="${1:-}"
+  local alertType=${2:-"info"}
+  local actionStatus=${3:-"unknown"}
+  local componentName=${4:-"not applicable"}
   local action=${5:-}
   local category=${6:-}
   local retries=${7:-}
   local task=${8:-}
   local actionDuration=${9:-}
+  local notificationTitle="$(cat ${profileConfigJsonPath} | jq -r '.notification_title | select(.!=null)')"
 
+  if [[ -z ${notificationTitle} ]]; then
+    notificationTitle="KX.AS.CODE"
+  fi
+  
   local emailAddress="$(cat ${profileConfigJsonPath} | jq -r '.notification_endpoints.email_address')"
 
   if [[ -n ${emailAddress} ]] && [[ "${emailAddress}" != "null" ]] && [[ -n ${message} ]]; then
 
-      log_debug 'echo "<html><body>'${message}'</body></html>" | mailx -a "From: Dev VM Notification <noreply@'${baseDomain}'>" -a "Content-type: text/html;" -s "['${alertType^^}'] Testing Notifications" '${emailAddress}''
+      log_trace 'echo "<html><body>'${message}'</body></html>" | mailx -a "From: Dev VM Notification <noreply@'${baseDomain}'>" -a "Content-type: text/html;" -s "['${alertType^^}'] Testing Notifications" '${emailAddress}''
 
       echo "<html><body>${message}</body></html>" | mailx -a "From: Dev VM Notification <noreply@${baseDomain}>" -a "Content-type: text/html;" -s "[${alertType^^}] Testing Notifications" ${emailAddress}
 
   fi
-
-  # Call common function to execute common function start commands, such as unsetting verbose output etc
-  functionEnd
 
 }
