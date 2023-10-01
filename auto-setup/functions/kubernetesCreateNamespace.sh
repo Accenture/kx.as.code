1a1d35e4180b29139017da6e8cb56fb305f4233a
Patrick Delamere <patdel76@gmail.com>
2023-10-01 11:50:35 +0200

    [#546] Added error and log handling for all scripts and functions

--- auto-setup/functions/kubernetesCreateNamespace.sh
+++ auto-setup/functions/kubernetesCreateNamespace.sh
@@ -1,14 +1,10 @@
 createKubernetesNamespace() {
 
-  # Call common function to execute common function start commands, such as setting verbose output etc
-  functionStart
-
   if [[ -n $(which kubectl || true) ]]; then
     if [[ -z ${namespace} ]] && [[ ${namespace} != "kube-system" ]] && [[ ${namespace} != "default"   ]]; then
         log_info "System namespace or namespace defined for \"${componentName}\" in metadata.json. Not creating namespace. Most likely intentional and not an issue"
-    fi
-    if [[ -n ${namespace} ]]; then
-        if [[ "$(kubectl get namespace ${namespace} --template={{.status.phase}})" != "Active" ]] && [[ ${namespace} != "kube-system" ]] && [[ ${namespace} != "default" ]]; then
+    else
+        if [[ "$(kubectl get namespace ${namespace} --template='{{.status.phase}}')" != "Active" ]] && [[ ${namespace} != "kube-system" ]] && [[ "${namespace}" != "default" ]]; then
             log_info "Namespace \"${namespace}\" does not exist. Creating"
             kubectl create namespace ${namespace}
             # Create secret to internal private registry
@@ -32,8 +28,5 @@ createKubernetesNamespace() {
   else
     log_debug "Kubectl not yet installed. Skipping namespace creation"
   fi
-
-  # Call common function to execute common function start commands, such as unsetting verbose output etc
-  functionEnd
 
 }
