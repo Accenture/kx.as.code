d9d810198368cd668b0298be313e68cd21f99982
Patrick Delamere <patdel76@gmail.com>
2023-10-01 11:09:36 +0200

    [#559] Added PyCharm

---
+++ auto-setup/dev_tools/pycharm/post_install_scripts/installPlugins.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+numberOfPluginsToInstall=$(($(cat ${componentMetadataJson} | jq '.plugins_to_install | length') -1))
+for (( i=0; i<=${numberOfPluginsToInstall}; i++ ))
+do
+
+    pluginAttributes=$(cat ${componentMetadataJson} | jq -r '.plugins_to_install['${i}']')
+
+    # Get details of zip to download from metadata.json
+    export targetFileName="intellij-$(echo ${pluginAttributes} | jq -r '.name')-plugin.zip"
+    export sha256Sum=$(echo ${pluginAttributes} | jq -r '.sha256Sum')
+    export downloadUrl=$(echo ${pluginAttributes} | jq -r '.downloadUrl')
+
+    # Debug log output
+    log_debug "targetFileName=${targetFileName}"
+    log_debug "sha256Sum=${sha256Sum}"
+    log_debug "downloadUrl=${downloadUrl}"
+
+    # Download content package from Artifactory
+    downloadFile "${downloadUrl}" \
+        "${sha256Sum}" \
+        "${installationWorkspace}/${targetFileName}" || rc=$?
+    if [[ ${rc} -ne 0 ]]; then
+        log_error "Downloading ${targetFileName} returned with (${rc}). Exiting with RC=${rc}"
+        message="ERROR: Could not download the IntelliJ plugin - ${targetFileName}"
+        notifyAllChannels "${message}" "error" "failed" "" "" "" "600000"
+        exit ${rc}
+    fi
+
+    # Unzip plugin to IntelliJ plugins folder
+    unzip ${installationWorkspace}/${targetFileName} -d /home/${baseUser}/intellij-idea/plugins
+
+done
