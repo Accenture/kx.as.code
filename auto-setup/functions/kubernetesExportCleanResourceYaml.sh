d49724490d7c0c1adb10c2fe84db88e2a2448e36
Patrick Delamere <patdel76@gmail.com>
2023-10-01 11:42:01 +0200

    [#574] Added function for cleanly exporting a Kubernetes resource

---
+++ auto-setup/functions/kubernetesExportCleanResourceYaml.sh
@@ -0,0 +1,10 @@
+kubernetesExportCleanResourceYaml() {
+
+    kubernetesResourceName=${1}
+    kubernetesResourceType=${2}
+    kubernetesResourceNamespace=${3}
+
+    kubectl get ${kubernetesResourceType} -n ${kubernetesResourceNamespace} ${kubernetesResourceName} -o yaml | \
+        yq 'del(.metadata.resourceVersion, .metadata.uid, .metadata.creationTimestamp, .metadata.selfLink, .metadata.managedFields, .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration", .status, .metadata.ownerReferences)' --yaml-output | tee ${installationWorkspace}/${kubernetesResourceName}-${kubernetesResourceType}-${kubernetesResourceNamespace}_export.yaml
+
+}
