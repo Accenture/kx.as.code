b20f76201e0c8dc0d1880a9ea91dc6bf6c8b686e
Patrick Delamere <patdel76@gmail.com>
2023-10-01 10:52:46 +0200

    [#548] Added Docker Registry tasks

--- auto-setup/core/docker-registry/available_tasks/pruneDockerRegistry.sh
+++ auto-setup/core/docker-registry/available_tasks/pruneDockerRegistry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Execute command
-kubectl exec $(kubectl get pod -l app=docker-registry -n docker-registry -o name) -c docker-registry -n docker-registry -- bin/registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
+# Call function to prune docker registry
+pruneDockerRegistry
 
 # Clean up local file system as well
 docker system prune --force
