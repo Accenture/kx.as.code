5b437a21ba2b82526b6d1e37ec4d0395dfb581cf
Patrick Delamere <patdel76@gmail.com>
2023-11-17 13:18:20 +0100

    [#612] Fixed config creation for logrotate.d

--- auto-setup/functions/createLogRotationConfiguration.sh
+++ auto-setup/functions/createLogRotationConfiguration.sh
@@ -1,27 +1,46 @@
 createLogRotationConfiguration() {
 
   local logFilePath=${1:-}
+  local maxSize=${2:-"50M"}
 
   if [[ -n ${logFilePath} ]]; then
 
-  log_debug "Creating log rotate configuration for ${logFilePath}"
+    set -o noglob # Avoid wildcard expansion
 
-  local filename=$(basename "${logFilePath}")
-  local logRotateShortFileName=$(basename $filename $(echo .$filename | cut -d'.' -f2))
+    local logFileDirectory=$(dirname "${logFilePath}")
 
-  # Add new log rotation configuration for log file
-  echo ''''${logFilePath}' {
-    copytruncate
-    daily
-    rotate 7
-    compress
-    missingok
-    size 50M
-  }
-  ''' | sed -e 's/^[ \t]*//' | /usr/bin/sudo tee /etc/logrotate.d/${logRotateShortFileName}
+    # Create directory in case it does not already exist, else the logrotate.d restart will fail
+    mkdir -p ${logFileDirectory}
 
-  # Restart Log Rotation service
-  /usr/bin/sudo systemctl restart logrotate.service 
+    log_debug "Creating log rotate configuration for ${logFilePath}"
+
+    local filename="$(basename \"${logFilePath}\")"
+    local logRotateShortFileName="$(basename $filename $(echo .$filename | cut -d'.' -f2) | sed 's/\*.*$//g')"
+
+    if [[ -z $(grep "${logFilePath}" /etc/logrotate.d/* ) ]]; then
+
+      # Add new log rotation configuration for log file
+      echo """${logFilePath} {
+        copytruncate
+        daily
+        rotate 7
+        compress
+        missingok
+        size ${maxSize}
+      }
+      """ | sed -e 's/^      //' | /usr/bin/sudo tee /etc/logrotate.d/${logRotateShortFileName}
+
+      if [[ -z $(grep "${logFileDirectory}" /lib/systemd/system/logrotate.service) ]]; then
+        # Ensure that log path is writable by logrotate service
+        echo "ReadWritePaths=${logFileDirectory}" | /usr/bin/sudo tee -a /lib/systemd/system/logrotate.service
+      fi
+
+      # Restart Log Rotation service
+      /usr/bin/sudo systemctl daemon-reload && /usr/bin/sudo systemctl start logrotate
+
+    set +o noglob # re-enable wildcard expansion
+
+    fi
 
   fi
 
