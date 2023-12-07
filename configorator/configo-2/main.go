5f798aed1366aba038fa11901505bc089cacb03e
burakkylp <bu.kayaalp@outlook.de>
2023-12-07 15:44:23 +0100

    add automatic string array detection in config template json to generate select options in config form.

--- configorator/configo-2/main.go
+++ configorator/configo-2/main.go
@@ -35,12 +35,22 @@ const minimalTemplate = `
             {{ if or (eq (printf "%T" $value) "string") (eq (printf "%T" $value) "bool") }}
                 <!-- If value is string or boolean, create an input field -->
                 <label for="{{ $key }}">{{ $key }}:</label>
-                <input type="{{ if eq (printf "%T" $value) "bool" }}checkbox{{ else }}text{{ end }}" 
-                       id="{{ $key }}" name="{{ $key }}" 
-                       {{ if eq (printf "%T" $value) "bool" }}{{ if $value }}checked{{ end }}{{ else }}placeholder="{{ $value }}"{{ end }}><br>
+                {{ if eq (printf "%T" $value) "bool" }}
+                    <input type="checkbox" id="{{ $key }}" name="{{ $key }}" {{ if $value }}checked{{ end }}><br>
+                {{ else }}
+                    <input type="text" id="{{ $key }}" name="{{ $key }}" placeholder="{{ $value }}"><br>
+                {{ end }}
+            {{ else if eq (printf "%T" $value) "[]interface {}" }}
+                <!-- If value is a string array, create a dropdown select field -->
+                <label for="{{ $key }}">{{ $key }}:</label>
+                <select id="{{ $key }}" name="{{ $key }}">
+                    {{ range $index, $item := $value }}
+                        <option value="{{ $item }}">{{ $item }}</option>
+                    {{ end }}
+                </select><br>
             {{ else if eq (printf "%T" $value) "map[string]interface {}" }}
                 <!-- If value is a nested map, recurse into it -->
-                <div style="">
+                <div style="margin-left: 20px;">
                     <h3>{{ $key }}</h3>
                     {{ template "fields" $value }}
                 </div>
